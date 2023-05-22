fb6654b3e4d565440e71166f58204ff3926bf661
Will Tebbutt <wt0881@my.bristol.ac.uk>
2023-05-22 14:57:25 +0100

    Tweak tests

--- test/algorithmic_differentiation.jl
+++ test/algorithmic_differentiation.jl
@@ -1,11 +1,10 @@
 @testset "algorithmic_differentiation" begin
     @testset for (f, x) in TestResources.UNARY_FUNCTIONS
-        val, tape = Taped.taped_trace(f, x; ctx=Taped.RMADContext())
+        val, tape = trace(f, x; ctx=Taped.RMADContext())
         @test val ≈ f(x)
         @test play!(tape, f, x) ≈ f(x)
         compiled_tape = compile(tape)
         @test compiled_tape(f, x) ≈ f(x)
-
 
         # You'll only be able to run ReverseDiff on the compiled tape if you modify the
         # implementation of `Umlaut.compile` to not restrict the type of the compiled
@@ -20,7 +19,7 @@
         f = TestResources.value_dependent_control_flow
         x = 5.0
         ctx = Taped.RMADContext()
-        val, tape = Taped.taped_trace(f, x, 5; ctx)
+        val, tape = trace(f, x, 5; ctx)
         @test_throws ErrorException play!(tape, f, x, 4)
     end
 
