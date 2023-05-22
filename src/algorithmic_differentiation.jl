925e7f03273e5f0653c466fcbc643d61204e7c3d
Will Tebbutt <wt0881@my.bristol.ac.uk>
2023-05-22 14:58:57 +0100

    Remove dead code

--- src/algorithmic_differentiation.jl
+++ src/algorithmic_differentiation.jl
@@ -45,9 +45,7 @@ get_cotangent(ctx::RMADContext, v) = NoTangent()
 function set_cotangent!(ctx::RMADContext, v::Umlaut.Variable, new_ȳ)
     is_constant(v) && return NoTangent()
     op = ctx.pbs[get_cotangent_index(ctx, v)]
-    @show cotangent(op.yȳ)
     op.yȳ = Dual(primal(op.yȳ), new_ȳ)
-    @show cotangent(op.yȳ)
     return new_ȳ
 end
 
@@ -162,8 +160,6 @@ function taped_rrule!!(::Dual{typeof(Umlaut.__new__)}, T, x...)
     return Dual(y, ȳ), __new__pullback!!
 end
 
-isprimitive(::RMADContext, ::typeof(Umlaut.__splatnew__), T, x...) = true 
-
 # Rules for built-ins.
 isprimitive(::RMADContext, ::typeof(Core.apply_type), x...) = true
 function taped_rrule!!(::Dual{typeof(Core.apply_type)}, x...)
@@ -174,9 +170,7 @@ end
 
 isprimitive(::RMADContext, ::F, x...) where {F<:typeof(Core.typeassert)} = true
 function taped_rrule!!(::Dual{typeof(Core.typeassert)}, x, T)
-    function typeassert_pullback!!(ȳ, ::NoTangent, x̄, ::NoTangent)
-        return NoTangent(), x̄, NoTangent()
-    end
+    typeassert_pullback!!(ȳ, ::NoTangent, x̄, ::NoTangent) = NoTangent(), x̄, NoTangent()
     yȳ = Dual(Core.typeassert(primal(x), primal(T)), cotangent(x))
     return yȳ, typeassert_pullback!!
 end
@@ -266,8 +260,6 @@ function taped_rrule!!(::Dual{typeof(setfield!)}, value, name, x)
     return yȳ, setfield!_pb!!
 end
 
-# @eval setindex!(A::Array{T}, x, i1::Int) where {T} = arrayset($(Expr(:boundscheck)), A, convert(T,x)::T, i1)
-
 
 # Rules to work around limitations in Umlaut.
 
@@ -317,94 +309,28 @@ end
 
 ChainRulesCore.@non_differentiable verify(::ConditionalCheck, ::Any)
 
-"""
-    trace!(t::Tracer, v_fargs)
-
-Trace call defined by variables in v_fargs.
-"""
-function taped_trace!(t::Umlaut.Tracer, v_fargs)
-    v_fargs = Umlaut.unsplat!(t, v_fargs)
-    # note: we need to extract IR before vararg grouping, which may change
-    # v_fargs, thus invalidating method search
-    ir = Umlaut.getcode(Umlaut.code_signature(t.tape.c, v_fargs)...)
-    v_fargs = Umlaut.group_varargs!(t, v_fargs)
-    frame = Umlaut.Frame(t.tape, ir, v_fargs...)
-    push!(t.stack, frame)
-    sparams = Umlaut.get_static_params(t, v_fargs)
-    bi = 1
-    prev_bi = 0
-    cf = nothing
-    while bi <= length(ir.cfg.blocks)
-        cf = Umlaut.trace_block!(t, ir, bi, prev_bi, sparams)
-        if isnothing(cf)
-            # fallthrough to the next block
-            prev_bi = bi
-            bi += 1
-        elseif cf isa Core.GotoIfNot
-            # conditional jump
-            cond_val = if cf.cond isa Umlaut.Argument || cf.cond isa Umlaut.SSAValue
-                # resolve tape var
-                v = t.tape[frame.ir2tape[cf.cond]].val
-                push!(t.tape, mkcall(verify, ConditionalCheck(v), frame.ir2tape[cf.cond]))
-                v
-            elseif cf.cond isa Bool
-                # literal condition (e.g. while true)
-                cf.cond
-            elseif cf.cond == Expr(:boundscheck)
-                # boundscheck expression must be evaluated on some later stage
-                # of interpretation that we don't have access to on this level
-                # so just skipping the check instead
-                true
-            else
-                exc = AssertionError(
-                    "Expected goto condition to be of type Argument, " *
-                    "SSAValue or Bool, but got $(cf.cond). \n\nFull IR: \n\n$(ir)\n"
-                )
-                throw(exc)
-            end
-            # if not cond, set i to destination, otherwise step forward
-            prev_bi = bi
-            bi = !cond_val ? cf.dest : bi + 1
-        elseif cf isa Core.GotoNode
-            # unconditional jump
-            prev_bi = bi
-            bi = cf.label
-        elseif cf isa Umlaut.ReturnNode
-            # global STATE = t, cf, ir
-            isdefined(cf, :val) || error("Reached unreachable")
-            res = cf.val
-            f, args... = Umlaut.var_values(v_fargs)
-            line = "return value from $f$(map(Core.Typeof, args))"
-            v = if res isa Umlaut.SSAValue || res isa Umlaut.Argument
-                val = frame.ir2tape[res]
-                val isa Umlaut.Variable ? val : push!(t.tape, Umlaut.Constant(Umlaut.promote_const_value(val); line))
-            elseif Meta.isexpr(res, :static_parameter, 1)
-                val = sparams[res.args[1]]
-                push!(t.tape, Constant(Umlaut.promote_const_value(val); line))
-            else
-                push!(t.tape, Umlaut.Constant(Umlaut.promote_const_value(res); line))
-            end
-            pop!(t.stack)
-            return v
-        else
-            error("Panic! Don't know how to handle control flow expression $cf")
-        end
-    end
-    pop!(t.stack)
-    # if no ReturnNode was encountered, use last op on the tape
-    return Umlaut.Variable(t.tape[V(end)])
-end
-
-function taped_trace(f, args...; ctx)
-    t = Umlaut.Tracer(Umlaut.Tape(ctx))
-    v_fargs = Umlaut.inputs!(t.tape, f, args...)
-    try
-        rv = taped_trace!(t, v_fargs)
-        t.tape.result = rv
-        return t.tape[t.tape.result].val, t.tape
-    catch
-        Umlaut.LATEST_TRACER[] = t
-        rethrow()
+function Umlaut.handle_gotoifnot_node!(
+    t::Umlaut.Tracer{<:RMADContext}, cf::Core.GotoIfNot, frame::Umlaut.Frame
+)
+    return if cf.cond isa Umlaut.Argument || cf.cond isa Umlaut.SSAValue
+        # resolve tape var
+        v = t.tape[frame.ir2tape[cf.cond]].val
+        push!(t.tape, mkcall(verify, ConditionalCheck(v), frame.ir2tape[cf.cond]))
+        v
+    elseif cf.cond isa Bool
+        # literal condition (e.g. while true)
+        cf.cond
+    elseif cf.cond == Expr(:boundscheck)
+        # boundscheck expression must be evaluated on some later stage
+        # of interpretation that we don't have access to on this level
+        # so just skipping the check instead
+        true
+    else
+        exc = AssertionError(
+            "Expected goto condition to be of type Argument, " *
+            "SSAValue or Bool, but got $(cf.cond). \n\nFull IR: \n\n$(ir)\n"
+        )
+        throw(exc)
     end
 end
 
@@ -494,15 +420,9 @@ function run_reverse_pass!(ctx::RMADContext, tape::Umlaut.Tape)
         op = ctx.pbs[n]
         is_input(op) && continue
         ȳ = cotangent(op.yȳ)
-        @show ȳ
         x̄s = map(Base.Fix1(get_cotangent, ctx), op.v_fargs)
-        @show op.pb!!
-        @show x̄s
         new_x̄s = op.pb!!(ȳ, x̄s...)
-        @show new_x̄s
         foreach((v, x̄) -> set_cotangent!(ctx, v, x̄), op.v_fargs, new_x̄s)
-        display(cotangent.(getfield.(ctx.pbs, :yȳ)))
-        println()
     end
     return nothing
 end
@@ -511,7 +431,7 @@ function value_and_derivative(f, x::Float64)
 
     # Run forwards-pass.
     ctx = RMADContext()
-    y, tape = taped_trace(f, x; ctx)
+    y, tape = trace(f, x; ctx)
 
     # Check that output is a Float64, and inform the user if it is not.
     y isa Float64 || throw(error("f(x) must return a Float64, but found a $(typeof(y))"))
