ee39635c10c264d05f845b89d00d4fd5eac338ac
Will Tebbutt <wt0881@my.bristol.ac.uk>
2023-10-09 12:53:04 +0100

    Factor out long-running tests into separate job

--- test/rrules/unrolled_function.jl
+++ test/rrules/unrolled_function.jl
@@ -61,64 +61,11 @@
         @test shadow(_out) == output_shadow(inst)
         @test all(map(==, new_dargs, input_shadows(inst)))
     end
-    @testset for (interface_only, f, x...) in vcat(
-        TestResources.TEST_FUNCTIONS,
-        [
-            (false, getindex, randn(5), 4),
-            (false, getindex, randn(5, 4), 1, 3),
-            (false, setindex!, randn(5), 4.0, 3),
-            (false, setindex!, randn(5, 4), 3.0, 1, 3),
-            (false, x -> getglobal(Main, :sin)(x), 5.0),
-            (false, x -> pointerref(bitcast(Ptr{Float64}, pointer_from_objref(Ref(x))), 1, 1), 5.0),
-            (false, (v, x) -> (pointerset(pointer(x), v, 2, 1); x), 3.0, randn(5)),
-            (false, x -> (pointerset(pointer(x), UInt8(3), 2, 1); x), rand(UInt8, 5)),
-            (false, x -> Ref(x)[], 5.0),
-            (false, x -> unsafe_load(bitcast(Ptr{Float64}, pointer_from_objref(Ref(x)))), 5.0),
-            (false, x -> unsafe_load(Base.unsafe_convert(Ptr{Float64}, x)), randn(5)),
-            (false, view, randn(5, 4), 1, 1),
-            (false, view, randn(5, 4), 2:3, 1),
-            (false, view, randn(5, 4), 1, 2:3),
-            (false, view, randn(5, 4), 2:3, 2:4),
-            (true, Array{Float64, 1}, undef, (1, )),
-            (true, Array{Float64, 2}, undef, (2, 3)),
-            (true, Array{Float64, 3}, undef, (2, 3, 4)),
-            (false, Array{Vector{Float64}, 1}, undef, (1, )),
-            (false, Array{Vector{Float64}, 2}, undef, (2, 3)),
-            (false, Array{Vector{Float64}, 3}, undef, (2, 3, 4)),
-            (false, Xoshiro, 123456),
-            (false, push!, randn(5), 3.0),
-        ],
-        map(n -> (false, map, sin, (randn(n)..., )), 1:7),
-        map(n -> (false, map, sin, randn(n)), 1:7),
-        map(n -> (false, x -> sin.(x), (randn(n)..., )), 1:7),
-        map(n -> (false, x -> sin.(x), randn(n)), 1:7),
-        vec(map(Iterators.product( # These all work fine, but take a long time to run.
-            [randn(3, 5), transpose(randn(5, 3)), adjoint(randn(5, 3))],
-            [
-                randn(3, 4),
-                transpose(randn(4, 3)),
-                adjoint(randn(4, 3)),
-                view(randn(5, 5), 1:3, 1:4),
-                transpose(view(randn(5, 5), 1:4, 1:3)),
-                adjoint(view(randn(5, 5), 1:4, 1:3)),
-            ],
-            [
-                randn(4, 5),
-                transpose(randn(5, 4)),
-                adjoint(randn(5, 4)),
-                view(randn(5, 5), 1:4, 1:5),
-                transpose(view(randn(5, 5), 1:5, 1:4)),
-                adjoint(view(randn(5, 5), 1:5, 1:4)),
-            ],
-        )) do (A, B, C)
-            (false, mul!, A, B, C, randn(), randn())
-        end),
-        TestResources.DIFFTESTS_FUNCTIONS[1:66], # SKIPPING SPARSE_LDIV
-        TestResources.DIFFTESTS_FUNCTIONS[68:89], # SKIPPING SPARSE_LDIV
-        TestResources.DIFFTESTS_FUNCTIONS[91:end], # SKIPPING SPARSE_LDIV
-    )
+    @testset for (interface_only, f, x...) in TestResources.TEST_FUNCTIONS
         @info "$(map(typeof, (f, x...)))"
-        test_taped_rrule!!(Xoshiro(123456), f, deepcopy(x)...; interface_only, perf_flag=:none)
+        test_taped_rrule!!(
+            Xoshiro(123456), f, deepcopy(x)...; interface_only, perf_flag=:none
+        )
     end
     @testset "acceleration $f" for (_, f, args...) in TestResources.TEST_FUNCTIONS
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -48,32 +48,41 @@ using .TestUtils:
     test_tangent,
     test_numerical_testing_interface
 
+const test_group = get(ENV, "TEST_GROUP", "basic")
+@show test_group
+
 @testset "Taped.jl" begin
-    include("tracing.jl")
-    include("acceleration.jl")
-    include("tangents.jl")
-    include("reverse_mode_ad.jl")
-    include("test_utils.jl")
-    @testset "rrules" begin
-        @info "avoiding_non_differentiable_code"
-        include(joinpath("rrules", "avoiding_non_differentiable_code.jl"))
-        @info "blas"
-        include(joinpath("rrules", "blas.jl"))
-        @info "builtins"
-        include(joinpath("rrules", "builtins.jl"))
-        @info "foreigncall"
-        include(joinpath("rrules", "foreigncall.jl"))
-        @info "lapack"
-        include(joinpath("rrules", "lapack.jl"))
-        @info "low_level_maths"
-        include(joinpath("rrules", "low_level_maths.jl"))
-        @info "misc"
-        include(joinpath("rrules", "misc.jl"))
-        @info "umlaut_internals_rules"
-        include(joinpath("rrules", "umlaut_internals_rules.jl"))
-        @info "battery_tests"
-        include(joinpath("rrules", "battery_tests.jl"))
-        @info "unrolled_function"
-        include(joinpath("rrules", "unrolled_function.jl"))
+    if test_group == "basic"
+        include("tracing.jl")
+        include("acceleration.jl")
+        include("tangents.jl")
+        include("reverse_mode_ad.jl")
+        include("test_utils.jl")
+        @testset "rrules" begin
+            @info "avoiding_non_differentiable_code"
+            include(joinpath("rrules", "avoiding_non_differentiable_code.jl"))
+            @info "blas"
+            include(joinpath("rrules", "blas.jl"))
+            @info "builtins"
+            include(joinpath("rrules", "builtins.jl"))
+            @info "foreigncall"
+            include(joinpath("rrules", "foreigncall.jl"))
+            @info "lapack"
+            include(joinpath("rrules", "lapack.jl"))
+            @info "low_level_maths"
+            include(joinpath("rrules", "low_level_maths.jl"))
+            @info "misc"
+            include(joinpath("rrules", "misc.jl"))
+            @info "umlaut_internals_rules"
+            include(joinpath("rrules", "umlaut_internals_rules.jl"))
+            @info "battery_tests"
+            include(joinpath("rrules", "battery_tests.jl"))
+            @info "unrolled_function"
+            include(joinpath("rrules", "unrolled_function.jl"))
+        end
+    elseif test_group == "extended"
+        include(joinpath("rrules", "integration_testing.jl"))
+    else
+        throw(error("test_group=$(test_group) is not recognised"))
     end
 end
