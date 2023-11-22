cefcaf3c1c0923832bab83a5e3b986cbdb100aa3
Will Tebbutt <wt0881@my.bristol.ac.uk>
2023-11-22 14:02:32 +0000

    Add test case for IdDict get rrule

--- test/rrules/iddict.jl
+++ test/rrules/iddict.jl
@@ -13,6 +13,7 @@
         (false, :none, setindex!, IdDict(true => 5.0, false => 4.0), 3.0, false),
         (false, :none, setindex!, IdDict(true => 5.0), 3.0, false),
         (false, :none, get, IdDict(true => 5.0, false => 4.0), false, 2.0),
+        (false, :none, get, IdDict(true => 5.0), false, 2.0),
         (false, :none, getindex, IdDict(true => 5.0, false => 4.0), true),
     ]
         test_rrule!!(Xoshiro(123456), f, x...; interface_only, perf_flag)
