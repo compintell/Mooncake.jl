7199a753f714721989af982f52f8cba10267ad31
Will Tebbutt <wt0881@my.bristol.ac.uk>
2023-12-11 17:02:21 +0000

    Add back benchmarking function

--- src/interpreted_function.jl
+++ src/interpreted_function.jl
@@ -1342,3 +1342,8 @@ is_primitive(::DefaultCtx, ::Type{<:Tuple{typeof(non_primitive), Any}}) = false
 contains_primitive(x) = @inline a_primitive(x)
 contains_non_primitive(x) = @inline non_primitive(x)
 contains_primitive_behind_call(x) = @inline contains_primitive(x)
+
+function to_benchmark(__rrule!!, df, dx)
+    out, pb!! = __rrule!!(df, dx...)
+    pb!!(tangent(out), tangent(df), map(tangent, dx)...)
+end
