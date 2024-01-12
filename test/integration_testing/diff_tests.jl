771ff611a4a96efffe6f0efbd7d21b63d835ed19
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-01-12 16:10:18 +0000

    Passing all relevant diff tests

--- test/integration_testing/diff_tests.jl
+++ test/integration_testing/diff_tests.jl
@@ -1,14 +1,15 @@
 @testset "diff_tests" begin
     interp = Taped.TInterp()
     @testset "$f, $(typeof(x))" for (interface_only, f, x...) in vcat(
-        TestResources.DIFFTESTS_FUNCTIONS[1:66], # SKIPPING SPARSE_LDIV
+        TestResources.DIFFTESTS_FUNCTIONS[1:31], # SKIPPING SPARSE_LDIV mat2num_4 and softmax due to `_apply_iterate` handling
+        TestResources.DIFFTESTS_FUNCTIONS[34:66], # SKIPPING SPARSE_LDIV
         TestResources.DIFFTESTS_FUNCTIONS[68:89], # SKIPPING SPARSE_LDIV
         TestResources.DIFFTESTS_FUNCTIONS[91:end], # SKIPPING SPARSE_LDIV
     )
         @info "$(map(typeof, (f, x...)))"
         rng = Xoshiro(123456)
         sig = Tuple{Core.Typeof(f), map(Core.Typeof, x)...}
-        in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp)
+        in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp);
         if interface_only
             in_f(f, deepcopy(x)...)
         else
@@ -17,6 +18,9 @@
             @test has_equal_data(in_f(f, x_cpy_1...), f(x_cpy_2...))
             @test has_equal_data(x_cpy_1, x_cpy_2)
         end
-        # TestUtils.test_interpreted_rrule!!(rng, f, deepcopy(x)...; interface_only, perf_flag=:none)
+        TestUtils.test_rrule!!(
+            Xoshiro(123456), in_f, f, x...;
+            perf_flag=:none, interface_only=false, is_primitive=false,
+        )
     end
 end
