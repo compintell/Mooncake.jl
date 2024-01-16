4fa39939e670a51504b091f5946fb35ee5cb1c71
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-01-16 14:48:39 +0000

    Uncomment benchmarking function

--- src/interpreter/test_cases.jl
+++ src/interpreter/test_cases.jl
@@ -13,7 +13,7 @@ contains_primitive(x) = @inline a_primitive(x)
 contains_non_primitive(x) = @inline non_primitive(x)
 contains_primitive_behind_call(x) = @inline contains_primitive(x)
 
-# function to_benchmark(__rrule!!, df, dx)
-#     out, pb!! = __rrule!!(df, dx...)
-#     pb!!(tangent(out), tangent(df), map(tangent, dx)...)
-# end
+function to_benchmark(__rrule!!::R, df::F, dx::X) where {R, F, X}
+    out, pb!! = __rrule!!(df, dx...)
+    pb!!(tangent(out), tangent(df), map(tangent, dx)...)
+end
