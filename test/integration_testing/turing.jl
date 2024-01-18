576f69237bbdcea2926b40d8359f70c8b7c690ca
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-01-18 09:51:57 +0000

    Change the way Turing is tested

--- test/integration_testing/turing.jl
+++ test/integration_testing/turing.jl
@@ -1,4 +1,4 @@
-using Turing
+using ReverseDiff, Turing
 
 @model function simple_model()
     y ~ Normal()
@@ -16,15 +16,12 @@ end
 end
 
 function build_turing_problem(rng, model_function, args...)
-
-    function test_function(x, model_function, args...)
-        model = model_function(args...)
-        ctx = Turing.DefaultContext()
-        vi = Turing.SimpleVarInfo(model)
-        vi_linked = Turing.link(vi, model)
-        ldp = Turing.LogDensityFunction(vi_linked, model, ctx)
-        return Turing.LogDensityProblems.logdensity(ldp, x)
-    end
+    model = model_function(args...)
+    ctx = Turing.DefaultContext()
+    vi = Turing.SimpleVarInfo(model)
+    vi_linked = Turing.link(vi, model)
+    ldp = Turing.LogDensityFunction(vi_linked, model, ctx)
+    test_function = Base.Fix1(Turing.LogDensityProblems.logdensity, ldp)
 
     m = model_function(args...)
     v = Turing.SimpleVarInfo(m)
@@ -45,26 +42,36 @@ end
         rng = sr(123)
         f, x = build_turing_problem(rng, model_function, args...)
 
-        x = (x, model_function, args...)
+        sig = Tuple{Core.Typeof(f), Core.Typeof(x)}
+        in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp);
+        if interface_only
+            in_f(f, deepcopy(x))
+        else
+            @test has_equal_data(in_f(f, deepcopy(x)), f(deepcopy(x)))
+        end
+        # display(@benchmark $f($x))
+        # println()
+        # display(@benchmark $in_f($f, $x))
+        # println()
+        TestUtils.test_rrule!!(
+            sr(123456), in_f, f, x;
+            perf_flag=:none, interface_only=true, is_primitive=false,
+        )
 
-        # Marked as broken while I figure out what to do with Turing tests.
-        @test_broken 1 == 0
+        # tape = ReverseDiff.GradientTape(f, x);
+        # ReverseDiff.gradient!(tape, x);
+        # result = zeros(size(x));
+        # ReverseDiff.gradient!(result, tape, x)
 
-        # # @show f(x...)
-        # sig = Tuple{Core.Typeof(f), map(Core.Typeof, x)...}
-        # in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp)
-        # if interface_only
-        #     in_f(f, deepcopy(x)...)
-        # else
-        #     @test has_equal_data(in_f(f, deepcopy(x)...), f(deepcopy(x)...))
-        # end
-        # # display(@benchmark $f($x...))
-        # # println()
-        # # display(@benchmark $in_f($f, $x...))
-        # # println()
-        # TestUtils.test_rrule!!(
-        #     sr(123456), in_f, f, x...;
-        #     perf_flag=:none, interface_only, is_primitive=false,
-        # )
+        # __rrule = Taped.build_rrule!!(in_f);
+        # codualed_args = map(zero_codual, (in_f, f, x));
+        # Taped.gradient(__rrule, codualed_args[1], codualed_args[2:end])[end]
+
+        # display(@benchmark ReverseDiff.gradient!($result, $tape, $x))
+        # println()
+        # display(@benchmark $__rrule($codualed_args...))
+        # println()
+        # display(@benchmark Taped.gradient($__rrule, $codualed_args[1], $(codualed_args[2:end])))
+        # println()
     end
 end
