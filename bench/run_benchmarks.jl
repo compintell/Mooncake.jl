12c72b1b89fa2b2021b0c1d138c17e71e7cef0d9
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-01-19 16:31:35 +0000

    Lower derived rule performance threshold

--- bench/run_benchmarks.jl
+++ bench/run_benchmarks.jl
@@ -83,7 +83,7 @@ function benchmark_hand_written_rrules!!(rng_ctor)
     return combine_results.(results, ranges, Ref(default_hand_written_ratios()))
 end
 
-default_derived_ratios() = (lb=1, ub=150)
+default_derived_ratios() = (lb=0.1, ub=150)
 
 function benchmark_derived_rrules!!(rng_ctor)
     rng = rng_ctor(123)
