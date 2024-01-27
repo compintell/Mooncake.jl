b03c7303df7b401c0cd5fd41ca75838b327d42a3
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-01-27 20:54:26 +0000

    Test a range of kernelmatrix functions

--- test/integration_testing/gp.jl
+++ test/integration_testing/gp.jl
@@ -2,22 +2,44 @@ using AbstractGPs, KernelFunctions
 
 @testset "gp" begin
     interp = Taped.TInterp()
-    @testset "kernelmatrix_diag $k, $(typeof(x))" for (k, x) in Iterators.product(
+    @testset "kernelmatrix_diag $k, $(typeof(x))" for (k, x1) in Iterators.product(
         Any[
             SEKernel(), Matern12Kernel(), Matern32Kernel(), Matern52Kernel()
         ],
         Any[
             randn(10), randn(1), ColVecs(randn(2, 11)), RowVecs(randn(9, 4)),
             range(0.0; step=0.1, length=11),
-        ]
+        ],
     )
-        f = kernelmatrix
-        x = (k, x, x)
-        sig = Tuple{typeof(f), map(typeof, x)...}
-        in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp)
-        TestUtils.test_rrule!!(
-            sr(123456), in_f, f, x...;
-            perf_flag=:none, interface_only=true, is_primitive=false,
-        )
+        @testset "ternary kernelmatrix" begin
+            f = kernelmatrix
+            x = (k, x1, x1)
+            sig = Tuple{typeof(f), map(typeof, x)...}
+            in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp)
+            TestUtils.test_rrule!!(
+                sr(123456), in_f, f, x...;
+                perf_flag=:none, interface_only=true, is_primitive=false,
+            )
+        end
+        @testset "ternary kernelmatrix_diag" begin
+            f = kernelmatrix_diag
+            x = (k, x1, x1)
+            sig = Tuple{typeof(f), map(typeof, x)...}
+            in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp)
+            TestUtils.test_rrule!!(
+                sr(123456), in_f, f, x...;
+                perf_flag=:none, interface_only=true, is_primitive=false,
+            )
+        end
+        @testset "binary kernelmatrix_diag" begin
+            f = kernelmatrix_diag
+            x = (k, x1)
+            sig = Tuple{typeof(f), map(typeof, x)...}
+            in_f = Taped.InterpretedFunction(DefaultCtx(), sig, interp)
+            TestUtils.test_rrule!!(
+                sr(123456), in_f, f, x...;
+                perf_flag=:none, interface_only=true, is_primitive=false,
+            )
+        end
     end
 end
