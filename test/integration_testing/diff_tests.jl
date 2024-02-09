c7ca087ef2ab8ad0381a16f29dd53d168c9197db
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-02-09 11:52:34 +0000

    Print out test number in DiffTests

--- test/integration_testing/diff_tests.jl
+++ test/integration_testing/diff_tests.jl
@@ -1,12 +1,12 @@
 @testset "diff_tests" begin
     interp = Taped.TInterp()
-    @testset "$f, $(_typeof(x))" for (interface_only, f, x...) in vcat(
+    @testset "$f, $(_typeof(x))" for (n, (interface_only, f, x...)) in enumerate(vcat(
         TestResources.DIFFTESTS_FUNCTIONS[1:31], # SKIPPING SPARSE_LDIV mat2num_4 and softmax due to `_apply_iterate` handling
         TestResources.DIFFTESTS_FUNCTIONS[34:66], # SKIPPING SPARSE_LDIV
         TestResources.DIFFTESTS_FUNCTIONS[68:89], # SKIPPING SPARSE_LDIV
         TestResources.DIFFTESTS_FUNCTIONS[91:end], # SKIPPING SPARSE_LDIV
-    )
-        @info "$(_typeof((f, x...)))"
+    ))
+        @info "$n: $(_typeof((f, x...)))"
         TestUtils.test_interpreted_rrule!!(
             sr(123456), f, x...;
             interp, perf_flag=:none, interface_only=false, is_primitive=false,
