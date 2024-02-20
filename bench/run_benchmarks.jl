0ff69b0cdcd4282bf46d0138123ef73046c4954d
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-02-20 17:40:46 +0000

    Initial benchmarking work

--- bench/run_benchmarks.jl
+++ bench/run_benchmarks.jl
@@ -1,7 +1,20 @@
 using Pkg
 Pkg.develop(path=joinpath(@__DIR__, ".."))
 
-using BenchmarkTools, CSV, DataFrames, Plots, Random, Taped, Test
+using
+    AbstractGPs,
+    BenchmarkTools,
+    CSV,
+    DataFrames,
+    Enzyme,
+    KernelFunctions,
+    LinearAlgebra,
+    Plots,
+    Random,
+    ReverseDiff,
+    Taped,
+    Test,
+    Zygote
 
 using Taped:
     CoDual,
@@ -14,44 +27,172 @@ using Taped:
 
 using Taped.TestUtils: _deepcopy, to_benchmark, set_up_gradient_problem
 
-function benchmark_rules!!(test_case_data, default_ratios)
+function zygote_to_benchmark(ctx, x::Vararg{Any, N}) where {N}
+    out, pb = Zygote._pullback(ctx, x...)
+    return pb(out)
+end
+
+function rd_to_benchmark!(result, tape, x)
+    return ReverseDiff.gradient!(result, tape, x)
+end
+
+# Test out the performance of a hand-written sum function, so we can be confident that there
+# is no rule. Note that ReverseDiff has a (seemingly not fantastic) hand-written rule for
+# sum.
+function _sum(x::AbstractArray{<:Real})
+    y = 0.0
+    n = 0
+    while n < length(x)
+        n += 1
+        y += x[n]
+    end
+    return y
+end
+
+# Zygote has rules for both sum and kron, so it's interesting to compare this against the
+# other frameworks because they don't have rules for kron, and maybe for not for sum.
+_kron_sum(x::AbstractMatrix{<:Real}, y::AbstractMatrix{<:Real}) = sum(kron(x, y))
+
+# Zygote (should) use the ChainRules projection functionality to handle the more interesting
+# types surrounding the arrays.
+function _kron_view_sum(x::AbstractMatrix{<:Real}, y::AbstractMatrix{<:Real})
+    return _kron_sum(view(x, 1:20, 1:20), UpperTriangular(y))
+end
+
+# No one has a rule for this.
+function _naive_map_sin_cos_exp(x::AbstractArray{<:Real})
+    y = similar(x)
+    for n in eachindex(x)
+        y[n] = sin(cos(exp(x[n])))
+    end
+    return sum(y)
+end
+
+# RD and Zygote have a rule for this.
+_map_sin_cos_exp(x::AbstractArray{<:Real}) = sum(map(x -> sin(cos(exp(x))), x))
+
+# Only Zygote has a rule for this.
+_broadcast_sin_cos_exp(x::AbstractArray{<:Real}) = sum(sin.(cos.(exp.(x))))
+
+# Different frameworks have rules for this to differing degrees.
+_simple_mlp(W2, W1, Y, X) = sum(abs2, Y - W2 * map(x -> x * (0 <= x), W1 * X))
+
+# Only Zygote and Taped can actually handle this. Note that Taped only has rules for BLAS
+# and LAPACK stuff, not explicit rules for things like the squared euclidean distance.
+# Consequently, Zygote is at a major advantage.
+_gp_lml(x, y, s) = logpdf(GP(SEKernel())(x, s), y)
+
+function _generate_gp_inputs()
+    x = collect(range(0.0; step=0.2, length=128))
+    s = 1.0
+    y = rand(GP(SEKernel())(x, s))
+    return x, y, s
+end
+
+"""
+    generate_inter_framework_tests()
+
+Constructs a set of benchmarks which can be used to compare between AD frameworks.
+Outputs a vector of tuples. Each tuples comprises a function (first element) and arguments
+at which its value and pullback should be computed (remaining elements).
+
+Arguments must comprise only scalars and arrays, and output must be either a scalar or
+an array.
+"""
+function generate_inter_framework_tests()
+    return Any[
+        (sum, randn(100)),
+        (_sum, randn(100)),
+        (_kron_sum, randn(20, 20), randn(40, 40)),
+        (_kron_view_sum, randn(40, 30), randn(40, 40)),
+        (_naive_map_sin_cos_exp, randn(10, 10)),
+        (_map_sin_cos_exp, randn(10, 10)),
+        (_broadcast_sin_cos_exp, randn(10, 10)),
+        (_simple_mlp, randn(128, 256), randn(256, 128), randn(128, 70), randn(128, 70)),
+        (_gp_lml, _generate_gp_inputs()...),
+    ]
+end
+
+function benchmark_rules!!(test_case_data, default_ratios, include_other_frameworks::Bool)
     test_cases = reduce(vcat, map(first, test_case_data))
     memory = map(x -> x[2], test_case_data)
     ranges = reduce(vcat, map(x -> x[3], test_case_data))
     GC.@preserve memory begin
         results = map(enumerate(test_cases)) do (n, args)
             @info "$n / $(length(test_cases))", _typeof(args)
-            suite = BenchmarkGroup()
+            suite = Dict()
 
             # Benchmark primal.
+            @info "primal"
             primals = map(x -> x isa CoDual ? primal(x) : x, args)
-            suite["primal"] = @benchmarkable(
-                (a[1])((a[2:end])...);
-                setup=(a = ($primals[1], _deepcopy($primals[2:end])...)),
+            suite["primal"] = @benchmark(
+                (a[1][])((a[2][])...);
+                setup=(a = (Ref($primals[1]), Ref(_deepcopy($primals[2:end])))),
             )
 
-            # Benchmark pullback.
+            # Benchmark AD via Taped.
+            @info "taped"
             rule, in_f = set_up_gradient_problem(args...)
             coduals = map(x -> x isa CoDual ? x : zero_codual(x), args)
-            suite["value_and_pb"] = @benchmarkable(
+            suite["taped"] = @benchmark(
                 to_benchmark($rule, zero_codual($in_f), $coduals...);
             )
 
-            return (args, BenchmarkTools.run(suite, verbose=true))
+            if include_other_frameworks
+
+                try
+                    @info "zygote"
+                    suite["zygote"] = @benchmark(
+                        zygote_to_benchmark($(Zygote.Context()), $primals...)
+                    )
+                catch
+                end
+
+                try
+                    @info "reverse diff"
+                    tape = ReverseDiff.GradientTape(primals[1], primals[2:end])
+                    compiled_tape = ReverseDiff.compile(tape)
+                    result = map(x -> randn(size(x)), primals[2:end])
+                    suite["rd"] = @benchmark(
+                        rd_to_benchmark!($result, $compiled_tape, $primals[2:end])
+                    )
+                catch
+                end
+
+                try
+                    @info "enzyme"
+                    dup_args = map(x -> Duplicated(x, randn(size(x))), primals[2:end])
+                    suite["enzyme"] = @benchmark(
+                        autodiff(Reverse, $primals[1], Active, $dup_args...)
+                    )
+                catch
+                end
+            end
+
+            return (args, suite)
         end
     end
     return combine_results.(results, ranges, Ref(default_ratios))
 end
 
 function combine_results(result, _range, default_range)
-    result_dict = result[2]
-    primal_time = time(minimum(result_dict["primal"]))
-    value_and_pb_time = time(minimum(result_dict["value_and_pb"]))
+    d = result[2]
+    primal_time = time(minimum(d["primal"]))
+    taped_time = time(minimum(d["taped"]))
+    zygote_time = in("zygote", keys(d)) ? time(minimum(d["zygote"])) : missing
+    rd_time = in("rd", keys(d)) ? time(minimum(d["rd"])) : missing
+    ez_time = in("enzyme", keys(d)) ? time(minimum(d["enzyme"])) : missing
     return (
-        tag=string(Taped._typeof((result[1]..., ))),
+        tag=string((result[1][1], map(Taped._typeof, result[1][2:end])...)),
         primal_time=primal_time,
-        value_and_pb_time=value_and_pb_time,
-        value_and_pb_ratio=value_and_pb_time / primal_time,
+        taped_time=taped_time,
+        taped_ratio=taped_time / primal_time,
+        zygote_time=zygote_time,
+        zygote_ratio=zygote_time / primal_time,
+        rd_time=rd_time,
+        rd_ratio=rd_time / primal_time,
+        enzyme_time=ez_time,
+        enzyme_ratio=ez_time / primal_time,
         range=_range === nothing ? default_range : _range,
     )
 end
@@ -72,7 +213,7 @@ function benchmark_hand_written_rrules!!(rng_ctor)
         ranges = map(x -> x[3], test_cases)
         return map(x -> x[4:end], test_cases), memory, ranges
     end
-    return benchmark_rules!!(test_case_data, (lb=1e-3, ub=25.0))
+    return benchmark_rules!!(test_case_data, (lb=1e-3, ub=25.0), false)
 end
 
 function benchmark_derived_rrules!!(rng_ctor)
@@ -83,7 +224,14 @@ function benchmark_derived_rrules!!(rng_ctor)
         ranges = map(x -> x[3], test_cases)
         return map(x -> x[4:end], test_cases), memory, ranges
     end
-    return benchmark_rules!!(test_case_data, (lb=0.1, ub=150))
+    return benchmark_rules!!(test_case_data, (lb=0.1, ub=150), false)
+end
+
+function benchmark_inter_framework_rules()
+    test_cases = generate_inter_framework_tests()
+    memory = []
+    ranges = fill(nothing, length(test_cases))
+    return benchmark_rules!!([(test_cases, memory, ranges)], (lb=0.1, ub=150), true)
 end
 
 function flag_concerning_performance(ratios)
@@ -112,6 +260,11 @@ function main()
         flag_concerning_performance(benchmark_hand_written_rrules!!(Xoshiro))
     elseif perf_group == "derived"
         flag_concerning_performance(benchmark_derived_rrules!!(Xoshiro))
+    elseif perf_group == "comparison"
+        results = benchmark_inter_framework_rules()
+        df = DataFrame(results)[:, [:tag, :taped_ratio, :zygote_ratio, :rd_ratio, :enzyme_ratio]]
+        display(df)
+        println()
     else
         throw(error("perf_group=$(perf_group) is not recognised"))
     end
