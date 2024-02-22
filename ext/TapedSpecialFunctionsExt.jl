f79ca207f30b7ab08efbf519f884e090819c6729
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-02-22 19:25:07 +0000

    Add some more SpecialFunctions rules and use macro

--- ext/TapedSpecialFunctionsExt.jl
+++ ext/TapedSpecialFunctionsExt.jl
@@ -1,16 +1,10 @@
 module TapedSpecialFunctionsExt
 
-    using ChainRulesCore, SpecialFunctions, Taped
+    using SpecialFunctions, Taped
 
-    import Taped: CoDual, rrule!!, @is_primitive, DefaultCtx
+    import Taped: @from_rrule, DefaultCtx
 
-    @is_primitive DefaultCtx Tuple{typeof(erfc), Float64}
-    function rrule!!(::CoDual{typeof(erfc)}, x::CoDual{Float64})
-        y, erfc_pb = rrule(erfc, primal(x))
-        function erfc_pb!!(dy, df, dx)
-            _, dx_inc = erfc_pb(dy)
-            return df, increment!!(dx, dx_inc)
-        end
-        return zero_codual(y), erfc_pb!!
-    end
+    @from_rrule DefaultCtx Tuple{typeof(airyai), Float64}
+    @from_rrule DefaultCtx Tuple{typeof(airyaix), Float64}
+    @from_rrule DefaultCtx Tuple{typeof(erfc), Float64}
 end
