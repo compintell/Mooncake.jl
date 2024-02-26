ae5380a5b9151e961258fc04df839f149a3f25a8
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-02-26 12:08:55 +0000

    Initial work on statement-specific transformations

---
+++ src/interpreter/s2s_reverse_mode_ad.jl
@@ -0,0 +1,174 @@
+#=
+    LineToADDataMap
+
+The "AD data associated to line n" is all of the data that is shared between the forwards-
+and reverse-passes associated to line number `n` in an `IRCode`.
+
+An `LineToADDataMap` is characterised by a `Dict{Int, Int}`. Each `key` is a line number in
+the primal `IRCode`, and each `value` is the unique position associated to it in a `Tuple`
+which gets shared between the forwards- and reverse-passes.
+
+It is important to maintain this map, rather than simply assuming that the identity function
+maps between the two, because many of the lines in primal `IRCode` do not have anything that
+they need to share between the forwards- and reverse-passes. This means that we can keep the
+size of the `Tuple` that must be shared between the forwards- and reverse-passes as small as
+possible. For example, `PhiNode`s and terminators never need to share information, nor do
+`:invoke` expressions which provably have the `NoPullback` pullback.
+=#
+struct LineToADDataMap
+    m::Dict{ID, Int}
+    LineToADDataMap() = new(Dict{ID, Int}())
+end
+
+#=
+    get_storage_location!(m::LineToADDataMap, line::ID)
+
+Return the location in the `Tuple` shared between the forwards- and reverse-passes
+associated to line `line`. If `m` does not already have an entry for `line`,
+create one, insert it into `m`, and return it.
+=#
+function get_storage_location!(m::LineToADDataMap, line::ID)
+    if !(line in keys(m.m))
+        m.m[line] = maximum(keys(m.m)) + 1
+    end
+    return m.m[line]
+end
+
+#=
+    ADInfo(terminator_block_id::ID, line_map::LineToADDataMap)
+
+This data structure is used to hold global information which gets passed around, in
+particular to `make_ad_stmts!`.
+
+- `terminator_block_id`: the ID of the block inserted to provide a unique exit point on the
+    forwards-pass
+- `line_map`: a `LineToADDataMap`.
+=#
+struct ADInfo
+    terminator_block_id::ID
+    line_map::LineToADDataMap
+end
+
+#=
+    ADStmtInfo
+
+Data structure which contains the result of `make_ad_stmts!`. Fields are
+- `fwds`: the instruction which runs the forwards-pass of AD
+- `rvs`: the instruction which runs the reverse-pass of AD / the pullback
+- `data`: data which must be made available to the forwards- and reverse-pass of AD
+
+For `rvs`, a value of `nothing` indicates that there should be no instruction associated
+to the primal statement in the pullback.
+
+For `data`, a value of `nothing` indicates that there is no data that needs to be shared
+between the forwards-pass and pullback, and that there is no need to allocate an element of
+the tuple which is shared between the forwards-pass and pullback to this primal line.
+=#
+struct ADStmtInfo
+    fwds
+    rvs
+    data
+end
+
+#=
+    make_ad_stmts(stmt, id::ID, info::ADInfo)::ADStmtInfo
+
+Every line in the primal code is associated to exactly one line in the forwards-pass of AD,
+and either one or zero lines in the pullback (many nodes do not need to appear in the
+pullback at all). This function specifies this translation for every type of node.
+
+Translates the statement `stmt`, associated to `id` in the primal, into a specification of
+what should happen for this statement in the forwards- and reverse-passes of AD, and what
+data should be shared between the forwards- and reverse-passes. Returns this in the form of
+an `ADStmtInfo`.
+
+`info` is a data structure containing various bits of information often required 
+
+Every type of node in the Julia SSAIR has its own method of `make_ad_stmts!`.
+=#
+function make_ad_stmts! end
+
+# `nothing` as a statement in Julia IR indicates the presence of a line which will later be
+# removed. We emit a no-op on both the forwards- and reverse-passes.
+make_ad_stmts!(::Nothing, ::ID, ::ADInfo) = ADStmtInfo(nothing, nothing, nothing)
+
+# If stmt.val is defined, then we have a regular return node, and should replace it with a
+# goto to the exit block on the forwards pass, and a no-op on the reverse-pass. If
+# stmts.val is not defined, then we have an unreachable node, and it should be left alone on
+# the forwards-pass, and be a no-op on the reverse-pass. An unreachable node can occur, for
+# example, immediately after a throw statement, because the compiler can be certain that
+# execution will end at the throw statement.
+function make_ad_stmts!(stmt::ReturnNode, ::ID, info::ADInfo)
+    if isdefined(stmt, :val)
+        return ADStmtInfo(IDGotoNode(info.terminator_block_id), nothing, nothing)
+    else
+        return ADStmtInfo(stmt, nothing, nothing)
+    end
+end
+
+make_ad_stmts!(stmt::IDGotoNode, ::ID, ::ADInfo) = ADStmtInfo(stmt, nothing, nothing)
+
+make_ad_stmts!(stmt::IDGotoIfNot, ::ID, ::ADInfo) = ADStmtInfo(stmt, nothing, nothing)
+
+make_ad_stmts!(stmt::IDPhiNode, ::ID, ::ADInfo) = ADStmtInfo(stmt, nothing, nothing)
+
+function make_ad_stmts!(stmt::Expr, line::ID, info::ADInfo)
+    if Meta.isexpr(stmt, :call)
+
+    elseif Meta.isexpr(stmt, :invoke)
+
+    elseif Meta.isexpr(stmt, :throw_undef_if_not)
+
+    elseif stmt.head in [
+        :boundscheck,
+        :code_coverage_effect,
+        :gc_preserve_begin,
+        :gc_preserve_end,
+        :loopinfo,
+        :leave,
+        :pop_exception,
+    ]
+
+    else
+        throw(error("Unrecognised expression $stmt"))
+    end
+end
+
+function make_ad_stmts!(stmt::PiNode, line::ID, info::ADInfo)
+
+end
+
+# Replace statement with construction of zero `CoDual`.
+function make_ad_stmts!(stmt::GlobalRef, ::ID, ::ADInfo)
+    return ADStmtInfo(Expr(:call, Taped.zero_codual, stmt), nothing, nothing)
+end
+
+# Replace statement with quote node for zero `CoDual`.
+function make_ad_stmts!(stmt::QuoteNode, ::ID, ::ADInfo)
+    return ADStmtInfo(QuoteNode(zero_codual(stmt.value)), nothing, nothing)
+end
+
+# Literal statement. Replace with zero `CoDual`. For example, `5` becomes a quote node
+# containing `CoDual(5, NoTangent())`, and `5.0` becomes a quote node `CoDual(5.0, 0.0)`.
+function make_ad_stmts!(stmt, ::ID, ::ADInfo)
+    return ADStmtInfo(QuoteNode(zero_codual(stmt)), nothing, nothing)
+end
+
+"""
+    build_rrule(::Type{C}, sig::Type{<:Tuple}) where {C}
+
+Returns and `OpaqueClosure` which is an `rrule!!` for `sig` in context `C`.
+"""
+function build_rrule(::Type{C}, sig::Type{<:Tuple}) where {C}
+
+    # Grab code associated to the primal.
+    interp = TapedInterpreter(C)
+    ir, Treturn = lookup_ir(interp, sig)
+
+    # Normalise the IR.
+    is_vararg, spnames = is_vararg_sig_and_sparam_names(sig)
+    ir = normalise!(ir, spnames)
+
+
+end
+
---
+++ test/interpreter/s2s_reverse_mode_ad.jl
@@ -0,0 +1,64 @@
+@testset "s2s_reverse_mode_ad" begin
+    @testset "make_ad_stmts!" begin
+        info = ADInfo(ID(), LineToADDataMap())
+        @testset "Nothing" begin
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(nothing, ID(), info),
+                ADStmtInfo(nothing, nothing, nothing),
+            )
+        end
+        @testset "ReturnNode" begin
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(ReturnNode(), ID(), info),
+                ADStmtInfo(ReturnNode(), nothing, nothing),
+            )
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(ReturnNode(5.0), ID(), info),
+                ADStmtInfo(IDGotoNode(info.terminator_block_id), nothing, nothing),
+            )
+        end
+        @testset "IDGotoNode" begin
+            stmt = IDGotoNode(ID())
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(stmt, ID(), info), ADStmtInfo(stmt, nothing, nothing),
+            )
+        end
+        @testset "IDGotoIfNot" begin
+            stmt = IDGotoIfNot(ID(), ID())
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(stmt, ID(), info), ADStmtInfo(stmt, nothing, nothing),
+            )
+        end
+        @testset "IDPhiNode" begin
+            stmt = IDPhiNode(ID[ID(), ID()], Any[ID(), 5.0])
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(stmt, ID(), info), ADStmtInfo(stmt, nothing, nothing),
+            )
+        end
+        @testset "GlobalRef" begin
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(GlobalRef(Base, :sin), ID(), info),
+                ADStmtInfo(
+                    Expr(:call, Taped.zero_codual, GlobalRef(Base, :sin)), nothing, nothing,
+                ),
+            )
+        end
+        @testset "QuoteNode" begin
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(QuoteNode("hi"), ID(), info),
+                ADStmtInfo(QuoteNode(CoDual("hi", NoTangent())), nothing, nothing),
+            )
+        end
+        @testset "literal" begin
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(5, ID(), info),
+                ADStmtInfo(QuoteNode(CoDual(5, NoTangent())), nothing, nothing),
+            )
+            @test TestUtils.has_equal_data(
+                make_ad_stmts!(0.0, ID(), info),
+                ADStmtInfo(QuoteNode(CoDual(0.0, 0.0)), nothing, nothing),
+            )
+        end
+    end
+
+end
