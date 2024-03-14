39455801eb04716b0fa8e00144d8ce7fa7aca22c
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-03-14 18:13:06 +0000

    Formatting and lgetfield tests

--- src/rrules/misc.jl
+++ src/rrules/misc.jl
@@ -67,20 +67,31 @@ function rrule!!(::CoDual{typeof(lgetfield)}, x::CoDual, ::CoDual{Val{f}}) where
 end
 
 # Specialise for non-differentiable arguments.
-function rrule!!(::CoDual{typeof(lgetfield)}, x::CoDual{<:Any, NoTangent}, ::CoDual{Val{f}}) where {f}
+function rrule!!(
+    ::CoDual{typeof(lgetfield)}, x::CoDual{<:Any, NoTangent}, ::CoDual{Val{f}}
+) where {f}
     return uninit_codual(getfield(primal(x), f)), NoPullback()
 end
 
 lgetfield(x, ::Val{f}, ::Val{order}) where {f, order} = getfield(x, f, order)
 
 @is_primitive MinimalCtx Tuple{typeof(lgetfield), Any, Any, Any}
-function rrule!!(::CoDual{typeof(lgetfield)}, x::CoDual, ::CoDual{Val{f}}, ::CoDual{Val{order}}) where {f, order}
-    lgetfield_pb!!(dy, df, dx, dsym, dorder) = df, increment_field!!(dx, dy, Val{f}()), dsym, dorder
+function rrule!!(
+    ::CoDual{typeof(lgetfield)}, x::CoDual, ::CoDual{Val{f}}, ::CoDual{Val{order}}
+) where {f, order}
+    function lgetfield_pb!!(dy, df, dx, dsym, dorder)
+        return df, increment_field!!(dx, dy, Val{f}()), dsym, dorder
+    end
     y = CoDual(getfield(primal(x), f), _get_tangent_field(primal(x), tangent(x), f))
     return y, lgetfield_pb!!
 end
 
-function rrule!!(::CoDual{typeof(lgetfield)}, x::CoDual{<:Any, NoTangent}, ::CoDual{Val{f}}, ::CoDual{Val{order}}) where {f, order}
+function rrule!!(
+    ::CoDual{typeof(lgetfield)},
+    x::CoDual{<:Any, NoTangent},
+    ::CoDual{Val{f}},
+    ::CoDual{Val{order}},
+) where {f, order}
     return uninit_codual(getfield(primal(x), f)), NoPullback()
 end
 
@@ -126,9 +137,7 @@ function generate_hand_written_rrule!!_test_cases(rng_ctor, ::Val{:misc})
     test_cases = Any[
         # Rules to avoid pointer type conversions.
         (
-            true,
-            :stability,
-            nothing,
+            true, :stability, nothing,
             +,
             CoDual(
                 bitcast(Ptr{Float64}, pointer_from_objref(_x)),
@@ -145,8 +154,14 @@ function generate_hand_written_rrule!!_test_cases(rng_ctor, ::Val{:misc})
 
         # Performance-rules that would ideally be completely removed.
         (false, :stability_and_allocs, nothing, size, randn(5, 4)),
-        (false, :stability_and_allocs, nothing, LinearAlgebra.lapack_size, 'N', randn(5, 4)),
-        (false, :stability_and_allocs, nothing, Base.require_one_based_indexing, randn(2, 3), randn(2, 1)),
+        (
+            false, :stability_and_allocs, nothing,
+            LinearAlgebra.lapack_size, 'N', randn(5, 4),
+        ),
+        (
+            false, :stability_and_allocs, nothing,
+            Base.require_one_based_indexing, randn(2, 3), randn(2, 1),
+        ),
         (false, :stability_and_allocs, nothing, in, 5.0, randn(4)),
         (false, :stability_and_allocs, nothing, iszero, 5.0),
         (false, :stability_and_allocs, nothing, isempty, randn(5)),
@@ -154,7 +169,10 @@ function generate_hand_written_rrule!!_test_cases(rng_ctor, ::Val{:misc})
         (false, :stability_and_allocs, nothing, sizeof, Float64),
         (false, :stability_and_allocs, nothing, promote_type, Float64, Float64),
         (false, :stability_and_allocs, nothing, LinearAlgebra.chkstride1, randn(3, 3)),
-        (false, :stability_and_allocs, nothing, LinearAlgebra.chkstride1, randn(3, 3), randn(2, 2)),
+        (
+            false, :stability_and_allocs, nothing,
+            LinearAlgebra.chkstride1, randn(3, 3), randn(2, 2),
+        ),
         (false, :allocs, nothing, Threads.nthreads),
 
         # Literal replacements for getfield.
@@ -166,26 +184,22 @@ function generate_hand_written_rrule!!_test_cases(rng_ctor, ::Val{:misc})
         (false, :stability_and_allocs, nothing, lgetfield, (a=5.0, b=4), Val(2)),
         (false, :stability_and_allocs, nothing, lgetfield, (a=5.0, b=4), Val(:a)),
         (false, :stability_and_allocs, nothing, lgetfield, (a=5.0, b=4), Val(:b)),
+        (false, :stability_and_allocs, nothing, lgetfield, 1:5, Val(:start)),
+        (false, :stability_and_allocs, nothing, lgetfield, 1:5, Val(:stop)),
 
         # Literal replacement for setfield!.
         (
-            false,
-            :stability_and_allocs,
-            nothing,
-            lsetfield!,
-            TestResources.MutableFoo(5.0, [1.0, 2.0]),
-            Val(:a),
-            4.0,
+            false, :stability_and_allocs, nothing,
+            lsetfield!, MutableFoo(5.0, [1.0, 2.0]), Val(:a), 4.0,
         ),
         (
-            false,
-            :stability_and_allocs,
-            nothing,
-            lsetfield!,
-            TestResources.FullyInitMutableStruct(5.0, [1.0, 2.0]),
-            Val(:y),
-            [1.0, 3.0, 4.0],
+            false, :stability_and_allocs, nothing,
+            lsetfield!, FullyInitMutableStruct(5.0, [1.0, 2.0]), Val(:y), [1.0, 3.0, 4.0],
         ),
+        (
+            false, :stability_and_allocs, nothing,
+            lsetfield!, NonDifferentiableFoo(5, false), Val(:x), 4,
+        )
     ]
     return test_cases, memory
 end
