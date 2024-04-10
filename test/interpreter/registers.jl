e69dc67bc9c41570e17f4d7b423188550d162336
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-04-10 14:34:11 +0100

    Excise register-related code

--- test/interpreter/registers.jl
+++
@@ -1,8 +0,0 @@
-@testset "registers" begin
-    @test Tapir.register_type(Float64) <: Tapir.AugmentedRegister{CoDual{Float64, Float64}}
-    @test Tapir.register_type(Bool) <: Tapir.AugmentedRegister{CoDual{Bool, NoTangent}}
-    @test Tapir.register_type(Any) == Tapir.AugmentedRegister
-    @test Tapir.register_type(Real) == Tapir.AugmentedRegister
-    @test ==(Tapir.register_type(Union{Float64, Float32}), Tapir.AugmentedRegister)
-    @test Tapir.register_type(Union{Float64, Bool}) <: Union{Tapir.AugmentedRegister, Bool}
-end
