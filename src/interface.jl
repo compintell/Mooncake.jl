a2230a3d8f4e34bd563aeb2702486b3c21376647
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-04-30 20:00:51 +0100

    Finish off interface update

--- src/interface.jl
+++ src/interface.jl
@@ -1,32 +1,33 @@
 """
-    value_and_pullback!!(rule, ȳ, f::CoDual, x::CoDual...)
+    __value_and_pullback!!(rule, ȳ, f::CoDual, x::CoDual...)
 
 In-place version of `value_and_pullback!!` in which the arguments have been wrapped in
 `CoDual`s. Note that any mutable data in `f` and `x` will be incremented in-place. As such,
 if calling this function multiple times with different values of `x`, should be careful to
 ensure that you zero-out the tangent fields of `x` each time.
 """
-function value_and_pullback!!(rule::R, ȳ::T, fx::Vararg{CoDual, N}) where {R, N, T}
-    out, pb!! = rule(fx...)
+function __value_and_pullback!!(rule::R, ȳ::T, fx::Vararg{CoDual, N}) where {R, N, T}
+    out, pb!! = rule(tuple_map(to_fwds, fx)...)
     @assert _typeof(tangent(out)) == fdata_type(T)
     increment!!(tangent(out), fdata(ȳ))
     v = copy(primal(out))
-    return v, tuple_map((f, r) -> tangent(tangent(f), r), fx, pb!!(rdata(ȳ)))
+    return v, tuple_map((f, r) -> tangent(fdata(tangent(f)), r), fx, pb!!(rdata(ȳ)))
 end
 
 """
-    value_and_gradient!!(rule, f::CoDual, x::CoDual...)
+    __value_and_gradient!!(rule, f::CoDual, x::CoDual...)
 
 Equivalent to `value_and_pullback(rule, 1.0, f, x...)` -- assumes `f` returns a `Float64`.
 """
-function value_and_gradient!!(rule::R, fx::Vararg{CoDual, N}) where {R, N}
-    return value_and_pullback!!(rule, 1.0, fx...)
+function __value_and_gradient!!(rule::R, fx::Vararg{CoDual, N}) where {R, N}
+    return __value_and_pullback!!(rule, 1.0, fx...)
 end
 
 """
     value_and_pullback!!(rule, ȳ, f, x...)
 
-Compute the value and pullback of `f(x...)`.
+Compute the value and pullback of `f(x...)`. `ȳ` must be a valid tangent for the primal
+return by `f(x...)`.
 
 `rule` should be constructed using `build_rrule`.
 
@@ -45,10 +46,10 @@ will yield the wrong result.
 *Note:* This method of `value_and_pullback!!` has to first call `zero_codual` on all of its
 arguments. This may cause some additional allocations. If this is a problem in your
 use-case, consider pre-allocating the `CoDual`s and calling the other method of this
-function.
+function. The `CoDual`s should be primal-tangent pairs (as opposed to primal-fdata pairs).
 """
 function value_and_pullback!!(rule::R, ȳ, fx::Vararg{Any, N}) where {R, N}
-    return value_and_pullback!!(rule, ȳ, map(zero_fcodual, fx)...)
+    return __value_and_pullback!!(rule, ȳ, tuple_map(zero_codual, fx)...)
 end
 
 """
@@ -57,5 +58,5 @@ end
 Equivalent to `value_and_pullback(rule, 1.0, f, x...)` -- assumes `f` returns a `Float64`.
 """
 function value_and_gradient!!(rule::R, fx::Vararg{Any, N}) where {R, N}
-    return value_and_gradient!!(rule, map(zero_fcodual, fx)...)
+    return __value_and_gradient!!(rule, tuple_map(zero_codual, fx)...)
 end
