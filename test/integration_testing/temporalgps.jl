f67448c4a55ec56c6ea92e5f39a9a730123289cf
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-05-13 16:42:18 +0100

    Remove commented out code

--- test/integration_testing/temporalgps.jl
+++ test/integration_testing/temporalgps.jl
@@ -24,41 +24,4 @@ temporalgps_logpdf_tester(k, x, y, s) = logpdf(build_gp(k)(x, s), y)
             interp, perf_flag=:none, interface_only=false, is_primitive=false
         )
     end
-
-
-    # x = range(-5.0; step=0.1, length=10_000)
-
-
-    # f = temporalgps_logpdf_tester
-    # x = (x, y, s)
-    # sig = _typeof((temporalgps_logpdf_tester, x...))
-    # @info "$sig"
-    # interp = Tapir.PInterp()
-    # TestUtils.test_derived_rule(
-    #     Xoshiro(123456), f, x...;
-    #     interp, perf_flag=:none, interface_only=false, is_primitive=false
-    # )
-
-    # codual_args = map(zero_codual, (f, x...))
-    # rule = Tapir.build_rrule(interp, sig)
-
-    # primal = @benchmark $f($x...)
-    # gradient = @benchmark(TestUtils.to_benchmark($rule, $codual_args...))
-
-    # println("primal")
-    # display(primal)
-    # println()
-
-    # println("gradient")
-    # display(gradient)
-    # println()
-
-    # @show time(gradient) / time(primal)
-
-    # # @profview run_many_times(100, f, x...)
-    # TestUtils.to_benchmark(rule, codual_args...)
-    # @profview run_many_times(10, TestUtils.to_benchmark, rule, codual_args...)
-    # Profile.clear()
-    # @profile run_many_times(10, TestUtils.to_benchmark, rule, codual_args...)
-    # pprof()
 end
