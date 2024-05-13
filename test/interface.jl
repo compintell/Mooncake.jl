029b720185955a5a4a3c9791134459c4513aa9ba
Will Tebbutt <wct23@cam.ac.uk>
2024-05-13 18:26:00 +0100

    Error handling for self-referential types (#144)
    * Add stack overflow handling
    
    * Add explanation to stack overflow handling
    
    * Bump patch

--- test/interface.jl
+++ test/interface.jl
@@ -1,3 +1,7 @@
+mutable struct MutableSelfRef
+    x::Any
+end
+
 @testset "interface" begin
     @testset "$(typeof((f, x...)))" for (ȳ, f, x...) in Any[
         (1.0, (x, y) -> x * y + sin(x) * cos(y), 5.0, 4.0),
@@ -16,5 +20,16 @@
         foo(x) = sin(cos(x))
         rule = build_rrule(foo, 5.0)
         @test_throws ArgumentError value_and_pullback!!(rule, 1.0, foo, CoDual(5.0, 0.0))
+    end
+    @testset "sensible error occurs when self-reference found" begin
+        rule = build_rrule(Tapir.PInterp(), Tuple{typeof(identity), MutableSelfRef})
+        v = MutableSelfRef(nothing)
+        v.x = v
+
+        # Check that zero_tangent for v does indeed cause a stack overflow.
+        @test_throws StackOverflowError zero_tangent(v)
+
+        # Check that we're catching the stack overflow.
+        @test_throws ErrorException value_and_pullback!!(rule, identity, v)
     end
 end
