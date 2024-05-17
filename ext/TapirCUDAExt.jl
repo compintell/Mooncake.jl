1c3a20bb57352deb414eef13ed6b90ad74c2dcb4
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-05-17 07:50:18 +0100

    Fix issues

--- ext/TapirCUDAExt.jl
+++ ext/TapirCUDAExt.jl
@@ -2,14 +2,17 @@ module TapirCUDAExt
 
     using CUDA, Tapir
 
-    import Tapir: MinimalCtx, rrule!!
+    import Tapir: MinimalCtx, rrule!!, @is_primitive, tangent_type, CoDual
 
-    @is_primitive MinimalCtx Tuple{Type{<:CuArray}, UndefInitializer, Vararg{Int, N}}
+    tangent_type(::Type{P}) where {P<:CuArray{<:Base.IEEEFloat}} = P
+
+    println("loading ext")
+    @is_primitive MinimalCtx Tuple{Type{<:CuArray}, UndefInitializer, Vararg{Int, N}} where {N}
     function rrule!!(
-        ::CoDual{Type{C}},
+        ::CoDual{Type{P}},
         ::CoDual{UndefInitializer},
         dims::CoDual{Int}...
-    ) where {P<:CuArray{<:IEEEFloat}}
+    ) where {P<:CuArray{<:Base.IEEEFloat}}
         y = CoDual(P(undef, dims), P(undef, dims))
         return y, NoPullback()
     end
--- test/integration_testing/cuda.jl
+++ test/integration_testing/cuda.jl
@@ -1,8 +1,9 @@
 using CUDA
 
 @testset "cuda" begin
+    interp = Tapir.TapirInterpreter()
     TestUtils.test_derived_rule(
-        sr(123456), CuArray{Float32, 1, CUDA.Mem.DeviceBuffer}, Undef, 256;
-        Tapir.PInterp(), perf_flag=:stability, interface_only=true, is_primitive=true,
+        sr(123456), CuArray{Float32, 1, CUDA.Mem.DeviceBuffer}, undef, 256;
+        interp, perf_flag=:stability, interface_only=true, is_primitive=true,
     )
 end
