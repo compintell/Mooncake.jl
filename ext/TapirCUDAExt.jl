e852358932d784e15fbd6e101d900e096897a65b
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-05-21 12:41:29 +0100

    Basic CUDA functionality

--- ext/TapirCUDAExt.jl
+++ ext/TapirCUDAExt.jl
@@ -1,23 +1,45 @@
 module TapirCUDAExt
 
-    using CUDA, Tapir
+    using LinearAlgebra, Random, Tapir
+
     using Base: IEEEFloat
+    using CUDA: CuArray, cu
 
-    import Tapir: MinimalCtx, rrule!!, @is_primitive, tangent_type, CoDual
+    import Tapir:
+        MinimalCtx,
+        rrule!!,
+        @is_primitive,
+        tangent_type,
+        zero_tangent,
+        randn_tangent,
+        increment!!,
+        set_to_zero!!,
+        _add_to_primal,
+        _diff,
+        _dot,
+        _scale,
+        TestUtils,
+        CoDual,
+        NoPullback
 
-    Tapir.tangent_type(::Type{P}) where {P<:CuArray{<:IEEEFloat}} = P
-    Tapir.zero_tangent(x::CuArray{<:IEEEFloat}) = zero(x)
-    function Tapir.randn_tangent(rng::AbstractRNG, x::CuArray{Float32})
-        return cu(randn(rng, size(x)...))
+    import Tapir.TestUtils: populate_address_map!, AddressMap, __increment_should_allocate
+
+    # Tell Tapir.jl how to handle CuArrays.
+
+    tangent_type(::Type{P}) where {P<:CuArray{<:IEEEFloat}} = P
+    zero_tangent(x::CuArray{<:IEEEFloat}) = zero(x)
+    function randn_tangent(rng::AbstractRNG, x::CuArray{Float32})
+        return cu(randn(rng, Float32, size(x)...))
     end
-    Tapir.TestUtils.has_equal_data(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x == y
-    Tapir.increment!!(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x .+= y
-    Tapir.set_to_zero!!(x::CuArray{<:IEEEFloat}) = x .= 0
-    Tapir._add_to_primal(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x + y
-    Tapir._diff(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x - y
-    Tapir._dot(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = Float64(dot(x, y))
-    Tapir._scale(x::Float64, y::P) where {T<:IEEEFloat, P<:CuArray{T}} = T(x) * y
-    function Tapir.TestUtils.populate_address_map!(m::AddressMap, p::CuArray, t::CuArray)
+    TestUtils.has_equal_data(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x == y
+    increment!!(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x .+= y
+    __increment_should_allocate(::Type{<:CuArray{<:IEEEFloat}}) = true
+    set_to_zero!!(x::CuArray{<:IEEEFloat}) = x .= 0
+    _add_to_primal(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x + y
+    _diff(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = x - y
+    _dot(x::P, y::P) where {P<:CuArray{<:IEEEFloat}} = Float64(dot(x, y))
+    _scale(x::Float64, y::P) where {T<:IEEEFloat, P<:CuArray{T}} = T(x) * y
+    function populate_address_map!(m::AddressMap, p::CuArray, t::CuArray)
         k = pointer_from_objref(p)
         v = pointer_from_objref(t)
         haskey(m, k) && (@assert m[k] == v)
@@ -25,14 +47,19 @@ module TapirCUDAExt
         return m
     end
 
-    println("loading ext")
-    @is_primitive MinimalCtx Tuple{Type{<:CuArray}, UndefInitializer, Vararg{Int, N}} where {N}
+    # Basic rules for operating on CuArrays.
+
+    @is_primitive(
+        MinimalCtx,
+        Tuple{Type{<:CuArray}, UndefInitializer, Vararg{Int, N}} where {N},
+    )
     function rrule!!(
-        ::CoDual{Type{P}},
-        ::CoDual{UndefInitializer},
+        p::CoDual{Type{P}},
+        init::CoDual{UndefInitializer},
         dims::CoDual{Int}...
     ) where {P<:CuArray{<:Base.IEEEFloat}}
-        y = CoDual(P(undef, dims), P(undef, dims))
-        return y, NoPullback()
+        _dims = map(primal, dims)
+        y = CoDual(P(undef, _dims), P(undef, _dims))
+        return y, NoPullback(p, init, dims...)
     end
 end
--- test/integration_testing/cuda.jl
+++ test/integration_testing/cuda.jl
@@ -13,6 +13,6 @@ using CUDA
     interp = Tapir.TapirInterpreter()
     TestUtils.test_derived_rule(
         sr(123456), CuArray{Float32, 1, CUDA.Mem.DeviceBuffer}, undef, 256;
-        interp, perf_flag=:stability, interface_only=true, is_primitive=true,
+        interp, perf_flag=:none, interface_only=true, is_primitive=true,
     )
 end
