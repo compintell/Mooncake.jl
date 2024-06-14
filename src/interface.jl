4a8e2cc6bb0520a6b0ed2f20ae07b6f5f01c62e3
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-06-14 13:43:13 +0100

    Add doctest to value_and_gradient

--- src/interface.jl
+++ src/interface.jl
@@ -74,11 +74,25 @@ end
 """
     value_and_gradient!!(rule, f, x...)
 
-Equivalent to `value_and_pullback(rule, 1.0, f, x...)` -- assumes `f` returns a `Float64`.
+Equivalent to `value_and_pullback!!(rule, 1.0, f, x...)`, and assumes `f` returns a
+`Float64`.
 
 *Note:* There are lots of subtle ways to mis-use `value_and_pullback!!`, so we generally
-recommend using `value_and_gradient!!` (this function) where possible. Its docstring is
-useful for understanding this function though.
+recommend using [`value_and_gradient!!`](@ref) (this function) where possible. The docstring for
+`value_and_pullback!!` is useful for understanding this function though.
+
+An example:
+```jldoctest
+f(x, y) = sum(x .* y)
+x = [2.0, 2.0]
+y = [1.0, 1.0]
+rule = build_rrule(f, x, y)
+value_and_gradient!!(rule, f, x, y)
+
+# output
+
+(4.0, (NoTangent(), [1.0, 1.0], [2.0, 2.0]))
+```
 """
 function value_and_gradient!!(rule::R, fx::Vararg{Any, N}) where {R, N}
     return __value_and_gradient!!(rule, __create_coduals(fx)...)
