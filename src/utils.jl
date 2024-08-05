a189437df37b697521029e07fb310a85c7ebf283
Will Tebbutt <wct23@cam.ac.uk>
2024-08-05 17:37:03 +0100

    Attempt to get better errors (#214)
    * Change how rules work
    
    * Fix typo
    
    * Attempt to get better errors
    
    * Print the right thing
    
    * Improve printing further
    
    * Simplify rule_type
    
    * Tidy up
    
    * Fix type stability issue
    
    * Bump patch

--- src/utils.jl
+++ src/utils.jl
@@ -4,7 +4,7 @@
 Central definition of typeof, which is specific to the use-required in this package.
 """
 _typeof(x) = Base._stable_typeof(x)
-_typeof(x::Tuple) = Tuple{map(_typeof, x)...}
+_typeof(x::Tuple) = Tuple{tuple_map(_typeof, x)...}
 _typeof(x::NamedTuple{names}) where {names} = NamedTuple{names, _typeof(Tuple(x))}
 
 """
