43688609ec20d768a98e8f7b20c5743863c59a60
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-09-11 09:24:19 +0100

    Enable all benchmarks

--- bench/run_benchmarks.jl
+++ bench/run_benchmarks.jl
@@ -140,21 +140,21 @@ an array.
 """
 function generate_inter_framework_tests()
     return Any[
-        # ("sum_1000", (sum, randn(1_000))),
-        # ("_sum_1000", (x -> _sum(identity, x), randn(1_000))),
-        # ("sum_sin_1000", (x -> sum(sin, x), randn(1_000))),
-        # ("_sum_sin_1000", (x -> _sum(sin, x), randn(1_000))),
-        # ("kron_sum", (_kron_sum, randn(20, 20), randn(40, 40))),
-        # ("kron_view_sum", (_kron_view_sum, randn(40, 30), randn(40, 40))),
-        # ("naive_map_sin_cos_exp", (_naive_map_sin_cos_exp, randn(10, 10))),
-        # ("map_sin_cos_exp", (_map_sin_cos_exp, randn(10, 10))),
-        # ("broadcast_sin_cos_exp", (_broadcast_sin_cos_exp, randn(10, 10))),
-        # (
-        #     "simple_mlp",
-        #     (_simple_mlp, randn(128, 256), randn(256, 128), randn(128, 70), randn(128, 70)),
-        # ),
-        # ("gp_lml", (_gp_lml, _generate_gp_inputs()...)),
-        # ("turing_broadcast_benchmark", build_turing_problem()),
+        ("sum_1000", (sum, randn(1_000))),
+        ("_sum_1000", (x -> _sum(identity, x), randn(1_000))),
+        ("sum_sin_1000", (x -> sum(sin, x), randn(1_000))),
+        ("_sum_sin_1000", (x -> _sum(sin, x), randn(1_000))),
+        ("kron_sum", (_kron_sum, randn(20, 20), randn(40, 40))),
+        ("kron_view_sum", (_kron_view_sum, randn(40, 30), randn(40, 40))),
+        ("naive_map_sin_cos_exp", (_naive_map_sin_cos_exp, randn(10, 10))),
+        ("map_sin_cos_exp", (_map_sin_cos_exp, randn(10, 10))),
+        ("broadcast_sin_cos_exp", (_broadcast_sin_cos_exp, randn(10, 10))),
+        (
+            "simple_mlp",
+            (_simple_mlp, randn(128, 256), randn(256, 128), randn(128, 70), randn(128, 70)),
+        ),
+        ("gp_lml", (_gp_lml, _generate_gp_inputs()...)),
+        ("turing_broadcast_benchmark", build_turing_problem()),
         ("large_single_block", (large_single_block, [0.9, 0.99])),
     ]
 end
