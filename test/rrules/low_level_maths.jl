80614b1fbc67a936254a3fb89f5fca1076c0274a
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-09-27 19:15:50 +0100

    Ensure that - is not a primitive

--- test/rrules/low_level_maths.jl
+++ test/rrules/low_level_maths.jl
@@ -1,3 +1,8 @@
 @testset "low_level_maths" begin
     TestUtils.run_rrule!!_test_cases(StableRNG, Val(:low_level_maths))
+
+    # We do not want to make this a primitive, because Base.sub_float is sufficient.
+    @test !Mooncake.is_primitive(DefaultCtx, Tuple{typeof(-), Float16, Float16})
+    @test !Mooncake.is_primitive(DefaultCtx, Tuple{typeof(-), Float32, Float32})
+    @test !Mooncake.is_primitive(DefaultCtx, Tuple{typeof(-), Float64, Float64})
 end    
