17d06c69d91a0994295c51094ead89d46b2b9039
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-10-04 11:17:12 +0100

    Formatting

--- test/interpreter/zero_like_rdata.jl
+++ test/interpreter/zero_like_rdata.jl
@@ -1,12 +1,6 @@
 @testset "zero_like_rdata" begin
     @testset "zero_like_rdata_from_type" begin
-        @testset "$P" for P in Any[
-            @NamedTuple{a},
-            Tuple{Any},
-            Float64,
-            Int,
-            Vector{Float64},
-        ]
+        @testset "$P" for P in [@NamedTuple{a}, Tuple{Any}, Float64, Int, Vector{Float64}]
             @test Mooncake.zero_like_rdata_from_type(P) isa Mooncake.zero_like_rdata_type(P)
         end
     end
