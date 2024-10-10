42ec66c27f6c0433a4917219f1f092aba420dd1a
willtebbutt <wtebbutt@turing.ac.uk>
2024-10-10 12:08:15 +0100

    Qualify import

--- test/rrules/memory.jl
+++ test/rrules/memory.jl
@@ -1,3 +1,5 @@
+using Mooncake: generate_data_test_cases
+
 @testset "memory" begin
     @testset "$(typeof(p))" for p in generate_data_test_cases(StableRNG, Val(:memory))
         TestUtils.test_data(sr(123), p)
