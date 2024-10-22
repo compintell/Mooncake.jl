415577a177d3e6cdb30cdd97ef401db0c1d5101e
Will Tebbutt <wct23@cam.ac.uk>
2024-10-22 07:50:56 +0100

    DI Integration Testing (#302)
    * DI Integration Testing
    
    * Make DI run
    
    * Fix typo
    
    * Add Pkg to DI tests
    
    * Active current directory
    
    * Add Pkg to Mooncake test deps
    
    * Avoid modifying the Project.toml each time the DI tests are run
    
    * Move CUDA dep to extra folder
    
    * Renaming DI file

--- test/ext/cuda/cuda.jl
+++ test/ext/cuda/cuda.jl
@@ -1,11 +1,14 @@
-using CUDA
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
+
+using CUDA, Mooncake, Test
 
 @testset "cuda" begin
 
     # Check we can operate on CuArrays.
     test_tangent(
-        Xoshiro(123456),
-        CuArray{Float32, 2, CUDA.DeviceMemory}(undef, 8, 8);
+        Xoshiro(123456), CuArray{Float32, 2, CUDA.DeviceMemory}(undef, 8, 8);
         interface_only=false,
     )
 
