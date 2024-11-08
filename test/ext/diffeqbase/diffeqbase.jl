e80f338d2b55833a706cca6007e7d50136cc5c48
willtebbutt <wtebbutt@turing.ac.uk>
2024-11-08 18:00:22 +0000

    Basics

--- test/ext/diffeqbase/diffeqbase.jl
+++ test/ext/diffeqbase/diffeqbase.jl
@@ -11,40 +11,33 @@ function lotka_volterra!(du, u, p, t)
     du[2] = dy = -δ * y + γ * x * y
 end
 
+u0 = [1.0, 1.0]
+tspan = (0.0, 1.0)
+p = [1.5, 1.0, 4.0, 1.0]
+
+# Have to use remake with a const global in order to get good performance.
+const __prob = ODEProblem(lotka_volterra!, u0, (0.0, 1.0), p)
+
 function build_and_solve(u0, tspan, p, sensealg)
-    prob = ODEProblem(lotka_volterra!, u0, (0.0, 1.0), p)
-    sol = DiffEqBase.solve(prob, Tsit5(); abstol=1e-14, reltol=1e-14, sensealg)
+    _prob = remake(__prob; u0, p)
+    sol = DiffEqBase.solve(_prob, Tsit5(); abstol=1e-14, reltol=1e-14, sensealg)
     return sum(sum(sol.u[end]))
 end
 
 @testset "diffeqbase" begin
-    u0 = [1.0, 1.0]
-    tspan = (0.0, 1.0)
-    p = [1.5, 1.0, 4.0, 1.0]
-
-    @testset "$sensealg" for sensealg in Any[
-        # ForwardSensitivity(), # falls over
-        ForwardDiffSensitivity(),
-        BacksolveAdjoint(),
-        GaussAdjoint(),
-        InterpolatingAdjoint(),
-        QuadratureAdjoint(),
-        # ReverseDiffAdjoint() # falls over -- some kind of stackoverflow
-        # TrackerAdjoint(), # stackoverflow
-        # ZygoteAdjoint() # falls over
-        # ForwardLSS(), # falls over -- might be user error on my part
-        # AdjointLSS(), # falls over
-        # NILSS(10, 10), # falls over
-        # NILSAS(10, 10, 15), # falls over
-    ]
+    vjps = [false, true, EnzymeVJP(), ZygoteVJP(), ReverseDiffVJP(), ReverseDiffVJP(true)]
+    reduced_vjps = [false, EnzymeVJP(), ReverseDiffVJP(), ReverseDiffVJP(true)]
+    @testset "$sensealg" for sensealg in vcat(
+        [ForwardDiffSensitivity()],
+        [BacksolveAdjoint(; autojacvec=vjp) for vjp in vjps],
+        [GaussAdjoint(; autojacvec=vjp) for vjp in reduced_vjps],
+        [InterpolatingAdjoint(; autojacvec=vjp) for vjp in vjps],
+        [QuadratureAdjoint(; autojacvec=vjp) for vjp in reduced_vjps],
+    )
+        @info sensealg
         Mooncake.TestUtils.test_rule(
             Xoshiro(123), build_and_solve, u0, tspan, p, sensealg;
             is_primitive=false, interface_only=false, debug_mode=false,
         )
     end
-
-    # display(@benchmark build_and_solve($u0, $tspan, $p))
-
-    # rule = Mooncake.build_rrule(build_and_solve, u0, tspan, p);
-    # display(@benchmark Mooncake.value_and_gradient!!($rule, build_and_solve, $u0, $tspan, $p))
 end
