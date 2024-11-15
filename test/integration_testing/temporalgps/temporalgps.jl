47a15110b105579c9d51b3de287e0fc17b9ec8c6
willtebbutt <wtebbutt@turing.ac.uk>
2024-11-15 15:29:57 +0000

    Add integration test for TemporalGPs with StepRangeLen

--- test/integration_testing/temporalgps/temporalgps.jl
+++ test/integration_testing/temporalgps/temporalgps.jl
@@ -13,6 +13,7 @@ temporalgps_logpdf_tester(k, x, y, s) = logpdf(build_gp(k)(x, s), y)
     xs = Any[
         collect(range(-5.0; step=0.1, length=1_000)),
         RegularSpacing(0.0, 0.1, 1_000),
+        range(-5.0; step=0.1, length=1_000),
     ]
     base_kernels = Any[Matern12Kernel(), Matern32Kernel()]
     kernels = vcat(base_kernels, [with_lengthscale(k, 1.1) for k in base_kernels])
