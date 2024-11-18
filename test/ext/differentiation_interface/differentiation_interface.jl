02f26754edc5a94b52270a697dc994084e4d99fe
willtebbutt <wtebbutt@turing.ac.uk>
2024-11-18 12:17:28 +0000

    Finish rename and fix loading

---
+++ test/ext/differentiation_interface/differentiation_interface.jl
@@ -0,0 +1,11 @@
+using Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
+
+using DifferentiationInterface, DifferentiationInterfaceTest
+import Mooncake
+
+test_differentiation(
+    [AutoMooncake(; config=nothing), AutoMooncake(; config=Mooncake.Config())];
+    excluded=SECOND_ORDER, logging=true,
+)
