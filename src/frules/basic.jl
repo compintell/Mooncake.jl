053a8bb57e0d5447e3bb6d4c398cebb2cb347e73
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-24 19:14:37 +0100

    Primitives already known

--- src/frules/basic.jl
+++ src/frules/basic.jl
@@ -1,11 +1,11 @@
 frule!!(f::F, args::Vararg{Dual,N}) where {F,N} = frule!!(zero_dual(f), args...)
 
-@is_primitive MinimalCtx Tuple{typeof(sin),IEEEFloat}
+# @is_primitive MinimalCtx Tuple{typeof(sin),IEEEFloat}
 function frule!!(::Dual{typeof(sin)}, x::Dual{<:Number})
     return Dual(sin(primal(x)), cos(primal(x)) * tangent(x))
 end
 
-@is_primitive MinimalCtx Tuple{typeof(cos),IEEEFloat}
+# @is_primitive MinimalCtx Tuple{typeof(cos),IEEEFloat}
 function frule!!(::Dual{typeof(cos)}, x::Dual{<:Number})
     return Dual(cos(primal(x)), -sin(primal(x)) * tangent(x))
 end
