7920fb7aefda82fbcf95c0b391111f2296ba6ea8
willtebbutt <wtebbutt@turing.ac.uk>
2024-12-10 17:12:48 +0000

    SpecialFunctions on F32

--- test/ext/special_functions/special_functions.jl
+++ test/ext/special_functions/special_functions.jl
@@ -7,61 +7,71 @@ using Mooncake.TestUtils: test_rule
 
 # Rules in this file are only lightly tester, because they are all just @from_rrule rules.
 @testset "special_functions" begin
-    @testset for (perf_flag, f, x...) in [
-        (:stability, airyai, 0.1),
-        (:stability, airyaix, 0.1),
-        (:stability, airyaiprime, 0.1),
-        (:stability, airybi, 0.1),
-        (:stability, airybiprime, 0.1),
-        (:stability_and_allocs, besselj0, 0.1),
-        (:stability_and_allocs, besselj1, 0.1),
-        (:stability_and_allocs, bessely0, 0.1),
-        (:stability_and_allocs, bessely1, 0.1),
-        (:stability_and_allocs, dawson, 0.1),
-        (:stability_and_allocs, digamma, 0.1),
-        (:stability_and_allocs, erf, 0.1),
-        (:stability_and_allocs, erf, 0.1, 0.5),
-        (:stability_and_allocs, erfc, 0.1),
-        (:stability_and_allocs, logerfc, 0.1),
-        (:stability_and_allocs, erfcinv, 0.1),
-        (:stability_and_allocs, erfcx, 0.1),
-        (:stability_and_allocs, logerfcx, 0.1),
-        (:stability_and_allocs, erfi, 0.1),
-        (:stability_and_allocs, erfinv, 0.1),
-        (:stability_and_allocs, gamma, 0.1),
-        (:stability_and_allocs, invdigamma, 0.1),
-        (:stability_and_allocs, trigamma, 0.1),
-        (:stability_and_allocs, polygamma, 3, 0.1),
-        (:stability_and_allocs, beta, 0.3, 0.1),
-        (:stability_and_allocs, logbeta, 0.3, 0.1),
-        (:stability_and_allocs, logabsgamma, 0.3),
-        (:stability_and_allocs, loggamma, 0.3),
-        (:stability_and_allocs, expint, 0.3),
-        (:stability_and_allocs, expintx, 0.3),
-        (:stability_and_allocs, expinti, 0.3),
-        (:stability_and_allocs, sinint, 0.3),
-        (:stability_and_allocs, cosint, 0.3),
-        (:stability_and_allocs, ellipk, 0.3),
-        (:stability_and_allocs, ellipe, 0.3),
+    @testset for (perf_flag, f, x...) in vcat(
+        map([Float64, Float32]) do P
+            return Any[
+                (:stability, airyai, P(0.1)),
+                (:stability, airyaix, P(0.1)),
+                (:stability, airyaiprime, P(0.1)),
+                (:stability, airybi, P(0.1)),
+                (:stability, airybiprime, P(0.1)),
+                (:stability_and_allocs, besselj0, P(0.1)),
+                (:stability_and_allocs, besselj1, P(0.1)),
+                (:stability_and_allocs, bessely0, P(0.1)),
+                (:stability_and_allocs, bessely1, P(0.1)),
+                (:stability_and_allocs, dawson, P(0.1)),
+                (:stability_and_allocs, digamma, P(0.1)),
+                (:stability_and_allocs, erf, P(0.1)),
+                (:stability_and_allocs, erf, P(0.1), P(0.5)),
+                (:stability_and_allocs, erfc, P(0.1)),
+                (:stability_and_allocs, logerfc, P(0.1)),
+                (:stability_and_allocs, erfcinv, P(0.1)),
+                (:stability_and_allocs, erfcx, P(0.1)),
+                (:stability_and_allocs, logerfcx, P(0.1)),
+                (:stability_and_allocs, erfi, P(0.1)),
+                (:stability_and_allocs, erfinv, P(0.1)),
+                (:stability_and_allocs, gamma, P(0.1)),
+                (:stability_and_allocs, invdigamma, P(0.1)),
+                (:stability_and_allocs, trigamma, P(0.1)),
+                (:stability_and_allocs, polygamma, 3, P(0.1)),
+                (:stability_and_allocs, beta, P(0.3), P(0.1)),
+                (:stability_and_allocs, logbeta, P(0.3), P(0.1)),
+                (:stability_and_allocs, logabsgamma, P(0.3)),
+                (:stability_and_allocs, loggamma, P(0.3)),
+                (:stability_and_allocs, expint, P(0.3)),
+                (:stability_and_allocs, expintx, P(0.3)),
+                (:stability_and_allocs, expinti, P(0.3)),
+                (:stability_and_allocs, sinint, P(0.3)),
+                (:stability_and_allocs, cosint, P(0.3)),
+                (:stability_and_allocs, ellipk, P(0.3)),
+                (:stability_and_allocs, ellipe, P(0.3)),
+            ]
+        end...,
         (:stability_and_allocs, logfactorial, 3),
-    ]
+    )
         test_rule(StableRNG(123456), f, x...; perf_flag)
     end
-    @testset for (perf_flag, f, x...) in [
-        (:none, logerf, 0.3, 0.5), # first branch
-        (:none, logerf, 1.1, 1.2), # second branch
-        (:none, logerf, -1.2, -1.1), # third branch
-        (:none, logerf, 0.3, 1.1), # fourth branch
-        (:allocs, SpecialFunctions.loggammadiv, 1.0, 9.0),
+    @testset for (perf_flag, f, x...) in vcat(
+        map([Float64, Float32]) do P
+            return Any[
+                (:none, logerf, P(0.3), P(0.5)), # first branch
+                (:none, logerf, P(1.1), P(1.2)), # second branch
+                (:none, logerf, P(-1.2), P(-1.1)), # third branch
+                (:none, logerf, P(0.3), P(1.1)), # fourth branch
+                (:allocs, SpecialFunctions.loggammadiv, P(1.0), P(9.0)),
+                (:allocs, logabsbeta, P(0.3), P(0.1)),
+            ]
+        end...,
+
+        # Functions which only support Float64.
         (:allocs, SpecialFunctions.gammax, 1.0),
         (:allocs, SpecialFunctions.rgammax, 3.0, 6.0),
         (:allocs, SpecialFunctions.rgamma1pm1, 0.1),
         (:allocs, SpecialFunctions.auxgam, 0.1),
-        (:allocs, logabsbeta, 0.3, 0.1),
         (:allocs, SpecialFunctions.loggamma1p, 0.3),
         (:allocs, SpecialFunctions.loggamma1p, -0.3),
         (:none, SpecialFunctions.lambdaeta, 5.0),
-    ]
+    )
         test_rule(StableRNG(123456), f, x...; perf_flag, is_primitive=false)
     end
 end
