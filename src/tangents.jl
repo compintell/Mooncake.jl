bc3d9f3278f682e8b57d7ba656233ebe063b212f
willtebbutt <wtebbutt@turing.ac.uk>
2024-12-13 11:35:25 +0000

    Only test memory on 1.11

--- src/tangents.jl
+++ src/tangents.jl
@@ -1044,8 +1044,8 @@ function tangent_test_cases()
         (a=3, b=randn(10)),
         (a=randn(10), b=randn(10)),
         (Base.TOML.ErrorType(1), NoTangent()), # Enum
-        fill!(Memory{Float64}(undef, 3), 3.0),
     ]
+    VERSION >= v"1.11" && push!(rel_test_cases, fill!(Memory{Float64}(undef, 3), 3.0))
     return vcat(
         map(x -> (false, x...), abs_test_cases),
         map(x -> (false, x), rel_test_cases),
