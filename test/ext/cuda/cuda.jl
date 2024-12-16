2e1117aed70b0400ab8467bb52575daf254938b2
Will Tebbutt <wt0881@my.bristol.ac.uk>
2024-12-16 12:55:57 +0000

    Proper cuda tests

--- test/ext/cuda/cuda.jl
+++ test/ext/cuda/cuda.jl
@@ -3,12 +3,12 @@ Pkg.activate(@__DIR__)
 Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
 
 using CUDA, JET, Mooncake, StableRNGs, Test
-using Mooncake.TestUtils: test_tangent, test_rule
+using Mooncake.TestUtils: test_data, test_rule
 
 @testset "cuda" begin
 
     # Check we can operate on CuArrays.
-    test_tangent(
+    test_data(
         StableRNG(123456),
         CuArray{Float32,2,CUDA.DeviceMemory}(undef, 8, 8);
         interface_only=false,
