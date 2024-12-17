6c705d270d0f4c2ef2c00cf1e123f13e6c4cf401
willtebbutt <wtebbutt@turing.ac.uk>
2024-12-17 10:52:41 +0000

    temporarily revert change

--- src/interpreter/s2s_reverse_mode_ad.jl
+++ src/interpreter/s2s_reverse_mode_ad.jl
@@ -254,7 +254,8 @@ Helper for [`reverse_data_ref_stmts`](@ref). Constructs a `Ref` whose element ty
 """
 @inline @generated function __make_ref(p::Type{P}) where {P}
     _P = @isdefined(P) ? P : _typeof(p)
-    return :(Ref{zero_like_rdata_type($_P)}(Mooncake.zero_like_rdata_from_type($_P)))
+    R = zero_like_rdata_type(_P)
+    return :(Ref{$R}(Mooncake.zero_like_rdata_from_type($_P)))
 end
 
 # This specialised method is necessary to ensure that `__make_ref` works properly for
