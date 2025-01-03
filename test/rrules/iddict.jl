bec5da465e9e2465653f482dd2e3801960ed75b6
willtebbutt <wtebbutt@turing.ac.uk>
2025-01-03 16:04:52 +0000

    Formatting

--- test/rrules/iddict.jl
+++ test/rrules/iddict.jl
@@ -1,7 +1,7 @@
 @testset "iddict" begin
     @testset "IdDict tangent functionality" begin
         p = IdDict(true => 5.0, false => 4.0)
-        T = IdDict{Bool, Float64}
+        T = IdDict{Bool,Float64}
         TestUtils.test_tangent(sr(123456), p, T; interface_only=false, perf=false)
         TestUtils.test_tangent_splitting(sr(123456), p)
     end
