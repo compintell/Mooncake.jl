69e40cdfcada3bbf70b728d817f1fb1938dbc196
Will Tebbutt <wt0881@my.bristol.ac.uk>
2025-01-08 12:06:32 +0000

    Tweaks to CUDA tests

--- test/ext/cuda/cuda.jl
+++ test/ext/cuda/cuda.jl
@@ -2,7 +2,7 @@ using Pkg
 Pkg.activate(@__DIR__)
 Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
 
-using CUDA, JET, Mooncake, StableRNGs, Test
+using AllocCheck, CUDA, JET, Mooncake, StableRNGs, Test
 using Mooncake.TestUtils: test_tangent_consistency, test_fwds_rvs_data
 
 @testset "cuda" begin
