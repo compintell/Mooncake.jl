792c6b46a519f09b350b540f72f8569f5b425b4c
willtebbutt <wtebbutt@turing.ac.uk>
2025-01-08 15:11:28 +0000

    Formatting

--- test/interpreter/contexts.jl
+++ test/interpreter/contexts.jl
@@ -4,11 +4,11 @@ using Mooncake: @is_primitive, DefaultCtx
 
 foo(x) = x
 
-@is_primitive DefaultCtx Tuple{typeof(foo), Float64}
+@is_primitive DefaultCtx Tuple{typeof(foo),Float64}
 
 end
 
 @testset "contexts" begin
-    @test Mooncake.is_primitive(DefaultCtx, Tuple{typeof(ContextsTestModule.foo), Float64})
-    @test !Mooncake.is_primitive(DefaultCtx, Tuple{typeof(ContextsTestModule.foo), Real})
+    @test Mooncake.is_primitive(DefaultCtx, Tuple{typeof(ContextsTestModule.foo),Float64})
+    @test !Mooncake.is_primitive(DefaultCtx, Tuple{typeof(ContextsTestModule.foo),Real})
 end
