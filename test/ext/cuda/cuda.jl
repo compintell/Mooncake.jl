51357db7b816c89ef146a7dbf2deb7733dc8332f
Will Tebbutt <wt0881@my.bristol.ac.uk>
2025-01-08 18:36:19 +0000

    Renable CuArray construction test

--- test/ext/cuda/cuda.jl
+++ test/ext/cuda/cuda.jl
@@ -3,7 +3,7 @@ Pkg.activate(@__DIR__)
 Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
 
 using AllocCheck, CUDA, JET, Mooncake, StableRNGs, Test
-using Mooncake.TestUtils: test_tangent_consistency, test_fwds_rvs_data
+using Mooncake.TestUtils: test_tangent_consistency, test_fwds_rvs_data, test_rule
 
 @testset "cuda" begin
 
@@ -12,14 +12,14 @@ using Mooncake.TestUtils: test_tangent_consistency, test_fwds_rvs_data
     test_tangent_consistency(StableRNG(123456), p; interface_only=false)
     test_fwds_rvs_data(StableRNG(123456), p)
 
-    # # Check we can instantiate a CuArray.
-    # test_rule(
-    #     StableRNG(123456),
-    #     CuArray{Float32,1,CUDA.DeviceMemory},
-    #     undef,
-    #     256;
-    #     interface_only=true,
-    #     is_primitive=true,
-    #     debug_mode=true,
-    # )
+    # Check we can instantiate a CuArray.
+    test_rule(
+        StableRNG(123456),
+        CuArray{Float32,1,CUDA.DeviceMemory},
+        undef,
+        256;
+        interface_only=true,
+        is_primitive=true,
+        debug_mode=true,
+    )
 end
