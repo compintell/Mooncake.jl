a7b1a7de25ecb5c56a5fdc9f8c2c8b4ec8aac610
Will Tebbutt <wt0881@my.bristol.ac.uk>
2025-02-03 19:25:36 +0000

    Fix typo

--- ext/MooncakeCUDAExt.jl
+++ ext/MooncakeCUDAExt.jl
@@ -36,7 +36,7 @@ const CuFloatArray = CuArray{<:IEEEFloat}
 
 Mooncake.@tt_effects tangent_type(::Type{P}) where {P<:CuFloatArray} = P
 function zero_tangent_internal(x::CuFloatArray, stackdict::Any)
-    haskey(stackdict, x) && return stackdict[x]::typeof(x)
+    haskey(stackdict, x) && return stackdict[x]::tangent_type(typeof(x))
     t = zero(x)
     stackdict[x] = t
     return t
