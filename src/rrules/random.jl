9d711d6f7932753de4d546eb8f462e4a5fe9adcd
Will Tebbutt <wt0881@my.bristol.ac.uk>
2025-03-04 11:37:26 +0000

    Tidy up more

--- src/rrules/random.jl
+++ src/rrules/random.jl
@@ -1,43 +1,26 @@
 const KnownRNGs = Union{MersenneTwister,RandomDevice,TaskLocalRNG,Xoshiro}
-const SpecialisedRNGs = Union{MersenneTwister,TaskLocalRNG,Xoshiro}
-
 @zero_adjoint MinimalCtx Tuple{typeof(randn),KnownRNGs}
 @zero_adjoint MinimalCtx Tuple{typeof(randexp),KnownRNGs}
 @zero_adjoint MinimalCtx Tuple{typeof(randn),KnownRNGs,Type{<:IEEEFloat}}
 @zero_adjoint MinimalCtx Tuple{typeof(randexp),KnownRNGs,Type{<:IEEEFloat}}
 
-# Needed to match specialised method in Random.jl.
-@is_primitive MinimalCtx Tuple{typeof(randn!),SpecialisedRNGs,Array{Float64}}
-function rrule!!(
-    ::CoDual{typeof(randn!)}, rng::CoDual{<:SpecialisedRNGs}, x::CoDual{<:Array{Float64}}
-)
-    current_x = copy(x.x)
-    current_dx = copy(x.dx)
-    randn!(rng.x, x.x)
-    x.dx .= 0.0
-    function randn!_adjoint(::NoRData)
-        x.x .= current_x
-        x.dx .= current_dx
-        return NoRData(), NoRData(), NoRData()
+const SpecialisedRNGs = Union{MersenneTwister,TaskLocalRNG,Xoshiro}
+for f in [randn!, randexp!]
+    @eval @is_primitive MinimalCtx Tuple{typeof($f),SpecialisedRNGs,Array{Float64}}
+    @eval function rrule!!(
+        ::CoDual{typeof($f)}, rng::CoDual{<:SpecialisedRNGs}, x::CoDual{<:Array{Float64}}
+    )
+        current_x = copy(x.x)
+        current_dx = copy(x.dx)
+        $f(rng.x, x.x)
+        x.dx .= 0.0
+        function rand_adjoint(::NoRData)
+            x.x .= current_x
+            x.dx .= current_dx
+            return NoRData(), NoRData(), NoRData()
+        end
+        return x, rand_adjoint
     end
-    return x, randn!_adjoint
-end
-
-# Needed to match specialised method in Random.jl.
-@is_primitive MinimalCtx Tuple{typeof(randexp!),SpecialisedRNGs,Array{Float64}}
-function rrule!!(
-    ::CoDual{typeof(randexp!)}, rng::CoDual{<:SpecialisedRNGs}, x::CoDual{<:Array{Float64}}
-)
-    current_x = copy(x.x)
-    current_dx = copy(x.dx)
-    randexp!(rng.x, x.x)
-    x.dx .= 0.0
-    function randexp!_adjoint(::NoRData)
-        x.x .= current_x
-        x.dx .= current_dx
-        return NoRData(), NoRData(), NoRData()
-    end
-    return x, randexp!_adjoint
 end
 
 function generate_hand_written_rrule!!_test_cases(rng_ctor, ::Val{:random})
