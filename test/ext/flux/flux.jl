43cb236caaa1528077cb23c88ee7ebf69c9f0aef
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2025-03-11 16:37:59 +0530

    Stability tests

--- test/ext/flux/flux.jl
+++ test/ext/flux/flux.jl
@@ -2,7 +2,7 @@ using Pkg
 Pkg.activate(@__DIR__)
 Pkg.develop(; path=joinpath(@__DIR__, "..", "..", ".."))
 
-using Mooncake, StableRNGs, Test, Flux
+using Mooncake, StableRNGs, Test, Flux, JET
 using Mooncake.TestUtils: test_rule
 
 @testset "flux" begin
