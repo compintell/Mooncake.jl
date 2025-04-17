f2a066df81895057eda9d4b6f25604194e5ce418
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-04-17 19:10:29 +0100

    Minor doc restructuring for clarity  (#553)
    * Update misc_internals_notes.md
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * Create reverse_mode_design
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * Update make.jl
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * Rename reverse_mode_design to reverse_mode_design.md
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    minor changes, @refs from reviews.
    
    Signed-off-by: Astitva Aggarwal <84859349+AstitvaAggarwal@users.noreply.github.com>
    
    * Update reverse_mode_design.md
    
    minor changes
    
    Signed-off-by: Astitva Aggarwal <84859349+AstitvaAggarwal@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    Signed-off-by: Astitva Aggarwal <84859349+AstitvaAggarwal@users.noreply.github.com>
    Co-authored-by: Astitva Aggarwal <84859349+AstitvaAggarwal@users.noreply.github.com>

--- docs/make.jl
+++ docs/make.jl
@@ -50,6 +50,7 @@ makedocs(;
             joinpath("developer_documentation", "developer_tools.md"),
             joinpath("developer_documentation", "ir_representation.md"),
             joinpath("developer_documentation", "forwards_mode_design.md"),
+            joinpath("developer_documentation", "reverse_mode_design.md"),
             joinpath("developer_documentation", "misc_internals_notes.md"),
             joinpath("developer_documentation", "internal_docstrings.md"),
         ],
