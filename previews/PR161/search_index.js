e04b42f21a9847c41943c711222965694d2e7a3a
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-04-17 11:51:53 +0100

    Delete previews directory
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- previews/PR161/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#Tapir.jl","page":"Tapir.jl","title":"Tapir.jl","text":"","category":"section"},{"location":"","page":"Tapir.jl","title":"Tapir.jl","text":"Documentation for Tapir.jl is on it's way!","category":"page"}]
-}
