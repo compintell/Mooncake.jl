8b4452435d7413bc39039a9ca3aeddef69a401e7
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-05-19 17:51:36 +0100

    Update blas.jl
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- test/rrules/blas.jl
+++ test/rrules/blas.jl
@@ -3,6 +3,8 @@
     TestUtils.run_rrule!!_test_cases(StableRNG, Val(:blas))
 
     @testset "mixed complex-real" begin
-        TestUtils.test_rule(StableRNG(123456), x->sum(complex(x)*x), rand(5,5), is_primitive=false)
+        TestUtils.test_rule(
+            StableRNG(123456), x->sum(complex(x)*x), rand(5, 5), is_primitive=false
+        )
     end
 end
