0e021c1f0dd087d667e3cfa374c80c3418108ae3
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2025-05-20 09:53:39 -0400

    Add arrayify for ReinterpretArray (#578)
    * add arrayify for ReinterpretArrays
    
    Should fix #577.
    
    * bump version
    
    * add regression test
    
    * Update blas.jl
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    Co-authored-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- src/rrules/blas.jl
+++ src/rrules/blas.jl
@@ -48,6 +48,11 @@ function arrayify(x::A, dx::FData) where {A<:Base.ReshapedArray{<:BlasRealFloat}
     _, _dx = arrayify(x.parent, dx.data.parent)
     return x, A(_dx, x.dims, x.mi)
 end
+function arrayify(x::Base.ReinterpretArray{T}, dx::FData) where {T<:BlasFloat}
+    _, _dx = arrayify(x.parent, dx.data.parent)
+    return x, reinterpret(T, _dx)
+end
+
 function arrayify(x::A, dx::DA) where {A,DA}
     msg =
         "Encountered unexpected array type in `Mooncake.arrayify`. This error is likely " *
