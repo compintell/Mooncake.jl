6990348e221e17c4697fc8e7f29d2a62fe10e343
Will Tebbutt <3628294+willtebbutt@users.noreply.github.com>
2025-06-04 20:29:35 +0100

    Forwards-over-reverse mwe

--- src/rrules/misty_closures.jl
+++ src/rrules/misty_closures.jl
@@ -1,6 +1,6 @@
 struct MistyClosureTangent
     captures_tangent::Any
-    dual_mc::MistyClosure
+    dual_mc::Any
 end
 
 _dual_mc(p::MistyClosure) = build_frule(get_interpreter(), p)
@@ -32,18 +32,18 @@ function _add_to_primal_internal(
     return replace_captures(p, new_captures)
 end
 
-function _diff_internal(c::MaybeCache, p::P, q::P) where {P<:MistyClosureTangent}
+function _diff_internal(c::MaybeCache, p::P, q::P) where {P<:MistyClosure}
     # Just assumes that the code associated to `p` is the same as that of `q`.
     captures_tangent = _diff_internal(c, p.oc.captures, q.oc.captures)
     return MistyClosureTangent(captures_tangent, _dual_mc(p))
 end
 
 function _dot_internal(c::MaybeCache, t::T, s::T) where {T<:MistyClosureTangent}
-    return _dot_internal(c, t.oc.captures, s.oc.captures)
+    return _dot_internal(c, t.captures_tangent, s.captures_tangent)
 end
 
 function _scale_internal(c::MaybeCache, a::Float64, t::T) where {T<:MistyClosureTangent}
-    captures_tangent = _scale_internal(c, a, t.oc.captures)
+    captures_tangent = _scale_internal(c, a, t.captures_tangent)
     return T(captures_tangent, t.dual_mc)
 end
 
@@ -56,24 +56,76 @@ end
 
 struct MistyClosureFData
     captures_fdata::Any
-    dual_mc::MistyClosure
+    dual_mc::Any
 end
 
-struct MistyClosureRData
-    captures_rdata::Any
+struct MistyClosureRData{Tr}
+    captures_rdata::Tr
 end
 
 fdata_type(::Type{<:MistyClosureTangent}) = MistyClosureFData
+fdata(t::MistyClosureTangent) = MistyClosureFData(fdata(t.captures_tangent), t.dual_mc)
+
 rdata_type(::Type{<:MistyClosureTangent}) = MistyClosureRData
+rdata(t::MistyClosureTangent) = MistyClosureRData(rdata(t.captures_tangent))
+
 @foldable function tangent_type(::Type{<:MistyClosureFData}, ::Type{<:MistyClosureRData})
     return MistyClosureTangent
 end
 function tangent(f::MistyClosureFData, r::MistyClosureRData)
-    return MistyClosureTangent(tangent(f.captures_data, r.captures_rdata), f.dual_mc)
+    return MistyClosureTangent(tangent(f.captures_fdata, r.captures_rdata), f.dual_mc)
 end
 
-function __verify_fdata_value(
-    ::IdDict{Any,Nothing}, p::MistyClosure, t::MistyClosureTangent
-)
+function __verify_fdata_value(::IdDict{Any,Nothing}, p::MistyClosure, t::MistyClosureFData)
     return nothing
+end
+_verify_rdata_value(p::MistyClosure, r::MistyClosureRData) = nothing
+
+zero_rdata(p::MistyClosure) = MistyClosureRData(zero_rdata(p.oc.captures))
+
+function increment!!(x::MistyClosureFData, y::MistyClosureFData)
+    return MistyClosureFData(increment!!(x.captures_fdata, y.captures_fdata), x.dual_mc)
+end
+
+function increment_internal!!(c::IncCache, x::MistyClosureRData, y::MistyClosureRData)
+    return MistyClosureRData(increment_internal!!(c, x.captures_rdata, y.captures_rdata))
+end
+
+function rrule!!(
+    ::CoDual{typeof(lgetfield)}, x::CoDual{P,F}, ::CoDual{Val{f}}
+) where {P<:MistyClosure,F<:MistyClosureFData,f}
+    misty_closure_getfield_rrule_exception()
+end
+
+function rrule!!(
+    ::CoDual{typeof(lgetfield)}, x::CoDual{P,F}, ::CoDual{Val{f}}, ::CoDual{Val{order}}
+) where {P<:MistyClosure,F<:MistyClosureFData,f,order}
+    misty_closure_getfield_rrule_exception()
+end
+
+function misty_closure_getfield_rrule_exception()
+    msg = "rrule!! for `lgetfield` and `getfield` not implemented for " *
+        "`MistyClosure`s. That is, you cannot currently query a field of a " *
+        "`MistyClosure` in code which you differentiate. If this is a " *
+        "problem for your use-case, please open an issue on the Mooncake.jl " *
+        "repository."
+    throw(UnhandledLanguageFeatureException(msg))
+end
+
+function rrule!!(::CoDual{typeof(_new_)}, p::CoDual{<:MistyClosure}, x::Vararg{CoDual})
+    misty_closure_getfield_rrule_exception()
+end
+
+function misty_closure_new_rrule_exception()
+    msg = "rrule!! for `_new_` not implemented for `MistyClosure`. That is, " *
+        "you cannot currently construct a `MistyClosure` in code that you " *
+        "differentiate. If this is a problem for your use-case, please open " *
+        "an issue on the Mooncake.jl repository."
+    throw(UnhandledLanguageFeatureException(msg))
+end
+
+@is_primitive MinimalCtx Tuple{MistyClosure, Vararg{Any, N}} where {N}
+function frule!!(f::Dual{<:MistyClosure}, x::Dual...)
+    dual_captures = Dual(primal(f).oc.captures, tangent(f).captures_tangent)
+    return tangent(f).dual_mc(dual_captures, x...)
 end
--- test/rrules/misty_closures.jl
+++ test/rrules/misty_closures.jl
@@ -1,5 +1,20 @@
 mc_foo(x) = 2x
 
+run_misty_closure(mc::Mooncake.MistyClosure, x::Float64) = mc(x)
+
+struct Foo
+    y::Float64
+end
+
+(f::Foo)(x) = getfield(f, 1) + x
+
+# Test cases for second derivative computation.
+quadratic(x) = x^2
+function low_level_gradient(rrule, f, x::Float64)
+    _, pb!! = rrule(zero_fcodual(f), zero_fcodual(x))
+    return pb!!(1.0)[2]
+end
+
 @testset "misty_closures" begin
 
     # Construct a sample MistyClosure.
@@ -10,9 +25,65 @@ mc_foo(x) = 2x
     @testset "tangent interface etc" begin
         rng = StableRNG(123456)
         TestUtils.test_tangent_interface(rng, mc)
+        TestUtils.test_tangent_splitting(rng, mc)
+        # Do not run the `test_rule_and_type_interactions` test suite for
+        # `MistyClosure`s as we do not implement rules for `getfield` / `_new_`.
     end
 
-    # fr = Mooncake.build_frule(Mooncake.get_interpreter(), mc)
-    # display(fr.fwd_oc.ir[])
-    # @show fr(zero_dual(mc_foo), zero_dual(5.0))
+    TestUtils.test_rule(
+        StableRNG(123), mc, 5.0;
+        interface_only=false,
+        is_primitive=true,
+        perf_flag=:none,
+        unsafe_perturb=true,
+        forward=true,
+    )
+    TestUtils.test_rule(
+        StableRNG(123), run_misty_closure, mc, 5.0;
+        interface_only=false,
+        is_primitive=false,
+        perf_flag=:none,
+        unsafe_perturb=true,
+        forward=true,
+    )
+
+    # Construct a MistyClosure which accesses its captures. We achieve this by collecting
+    # the IR associated to a callable type, and manipulating the types of various fields to
+    # ensure that the MistyClosure produced using its `IRCode` is valid.
+    ir = Base.code_ircode_by_type(Tuple{Foo, Float64})[1][1]
+    ir.argtypes[1] = Tuple{Float64}
+    mc2 = Mooncake.MistyClosure(ir, 5.0)
+    @test mc2(4.0) == 9.0
+    TestUtils.test_rule(
+        StableRNG(123), mc2, 4.0;
+        interface_only=false,
+        is_primitive=true,
+        perf_flag=:none,
+        unsafe_perturb=true,
+        forward=true,
+    )
+
+    # Construct a callable which performs reverse-mode, and apply forwards-mode over it.
+    rule = Mooncake.build_rrule(Tuple{typeof(quadratic), Float64})
+    TestUtils.test_rule(
+        StableRNG(123), low_level_gradient, rule, quadratic, 5.0;
+        interface_only=false,
+        is_primitive=false,
+        perf_flag=:none,
+        unsafe_perturb=true,
+        forward=true,
+    )
+
+    # Manually test that this correectly computes the second derivative.
+    frule = Mooncake.build_frule(
+        Mooncake.get_interpreter(),
+        Tuple{typeof(low_level_gradient), typeof(rule), typeof(quadratic), Float64}
+    )
+    result = frule(
+        zero_dual(low_level_gradient),
+        zero_dual(rule),
+        zero_dual(quadratic),
+        Mooncake.Dual(5.0, 1.0),
+    )
+    @test tangent(result) == 2.0
 end
