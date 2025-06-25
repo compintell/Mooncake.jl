54387b23199dfb60bf39f73b19a5ed4ed9153a89
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-06-25 01:42:55 +0100

    Update src/interpreter/contexts.jl
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- src/interpreter/contexts.jl
+++ src/interpreter/contexts.jl
@@ -71,7 +71,7 @@ macro is_primitive(Tctx, sig)
         # widen input argument types to Any for `is_noinline` to reduce false negatives 
         # inlining in type-unstable functions 
         sig_noinline = ($(esc(sig))[1], fill(Any, length($(esc(sig)))-1)...)
-        Mooncake.is_noinline(::Type{$(esc(ctx))}, ::Type{<:sig_noinline}) = true
+        Mooncake.is_noinline(::Type{$(esc(Tctx))}, ::Type{<:sig_noinline}) = true
         Mooncake.is_primitive(::Type{$(esc(Tctx))}, ::Type{<:$(esc(sig))}) = true
     end
 end
