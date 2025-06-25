18247b2ebc34771bc74f0d6ce060e35024857ee9
Astitva Aggarwal <astitvaaggarwalimportant@gmail.com>
2025-06-25 14:42:08 +0530

    rrule tests

--- src/rrules/low_level_maths.jl
+++ src/rrules/low_level_maths.jl
@@ -59,8 +59,9 @@ end
 
 @is_primitive MinimalCtx Tuple{typeof(Base.eps),<:IEEEFloat}
 function rrule!!(::CoDual{typeof(Base.eps)}, x::CoDual{P}) where {P<:IEEEFloat}
-    eps_pb!!(dy::P) = NoRData(), isinteger(log2(primal(x))) ? Inf : P(0.0)
-    return CoDual(Base.eps(primal(x)), NoFData()), eps_pb!!
+    y = Base.eps(primal(x))
+    eps_pb!!(dy::P) = NoRData(), isinteger(log2(primal(x))) ? P(Inf) : zero(y)
+    return zero_fcodual(y), eps_pb!!
 end
 
 rand_inputs(rng, P::Type{<:IEEEFloat}, f, arity) = randn(rng, P, arity)
@@ -113,6 +114,8 @@ function generate_hand_written_rrule!!_test_cases(rng_ctor, ::Val{:low_level_mat
     push!(test_cases, (false, :stability_and_allocs, nothing, exp, 1.0f1))
     push!(test_cases, (false, :stability_and_allocs, nothing, ^, 4.0, 5.0))
     push!(test_cases, (false, :stability_and_allocs, nothing, ^, 4.0f0, 5.0f0))
+    # push!(test_cases, (false, :stability_and_allocs, nothing, Base.eps, 4.0f0)) correctness tests fail as we compare against FDM, run manually to verify
+    push!(test_cases, (false, :stability_and_allocs, nothing, Base.eps, 5.0f0))
     memory = Any[]
     return test_cases, memory
 end
