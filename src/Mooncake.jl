3e87ac690a8738a85209013198e69ee1894a0830
MilesCranmer <miles.cranmer@gmail.com>
2025-06-28 21:33:59 +0100

    deps: remove DD as direct dependency

--- src/Mooncake.jl
+++ src/Mooncake.jl
@@ -38,7 +38,31 @@ using Core.Intrinsics: pointerref, pointerset
 using LinearAlgebra.BLAS: @blasfunc, BlasInt, trsm!, BlasFloat
 using LinearAlgebra.LAPACK: getrf!, getrs!, getri!, trtrs!, potrf!, potrs!
 using FunctionWrappers: FunctionWrapper
-using DispatchDoctor: @stable, @unstable
+using Preferences: load_preference, get_uuid
+
+
+const DD_ENABLED = let uuid = get_uuid(@__MODULE__)
+    mode = load_preference(uuid, "dispatch_doctor_mode")
+
+    mode ∉ (nothing, "disable")
+end
+@static if DD_ENABLED
+    # We avoid making DispatchDoctor a direct dependency to avoid
+    # a circular dependency, since DispatchDoctor has an extension
+    # that declares zero adjoint for internal calls.
+    using DispatchDoctor: DispatchDoctor
+end
+macro stable(args...)
+    @static if DD_ENABLED
+        esc(:($(DispatchDoctor).@stable($(args...))))
+    else
+        esc(args[end])
+    end
+end
+macro unstable(args...)
+    esc(args[end])
+end
+
 
 # Needs to be defined before various other things.
 function _foreigncall_ end
--- src/test_utils.jl
+++ src/test_utils.jl
@@ -159,8 +159,8 @@ using Mooncake:
     fdata,
     NoRData,
     rdata_type,
-    rdata
-using Preferences: load_preference, get_uuid
+    rdata,
+    DD_ENABLED
 
 struct Shim end
 
@@ -1076,12 +1076,6 @@ function test_get_tangent_field_performance(t::Union{MutableTangent,Tangent})
         @inferred _get_tangent_field(t, s)
         @test count_allocs(_get_tangent_field, t, s) == 0
     end
-end
-
-const DD_ENABLED = let uuid = get_uuid(@__MODULE__)
-    mode = load_preference(uuid, "dispatch_doctor_mode")
-
-    mode ∉ (nothing, "disable")
 end
 
 # Function barrier to ensure inference in value types.
