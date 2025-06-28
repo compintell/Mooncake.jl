a1c881ddc6f29bcafc9ac7af45529f923ea3664f
MilesCranmer <miles.cranmer@gmail.com>
2025-06-28 18:38:23 +0100

    refactor: type stable _diff_internal

--- src/tangents.jl
+++ src/tangents.jl
@@ -123,18 +123,37 @@ end
     return Expr(:call, :tuple, tangent_field_types_exprs(P)...)
 end
 
-@unstable function build_tangent(::Type{P}, fields...) where {P}
-    fields = map(enumerate(tangent_field_types(P))) do (n, tt)
-        tt <: PossiblyUninitTangent && return n <= length(fields) ? tt(fields[n]) : tt()
-        return fields[n]
+function build_tangent(::Type{P}, fields::Vararg{Any,N}) where {P,N}
+    TP = tangent_type(P)
+    ftypes = tangent_field_types(P)
+    fnames = fieldnames(P)
+    return _build_tangent_cartesian(TP, fields, ftypes, Val(fnames), Val(length(ftypes)))::TP
+end
+@generated function _build_tangent_cartesian(
+    ::Type{TP}, fields::Tuple{Vararg{Any,N}}, ftypes, ::Val{fnames}, ::Val{nfields}
+) where {TP,N,fnames,nfields}
+    quote
+        full_fields = Base.Cartesian.@ntuple(
+            $nfields, n -> let
+                tt = ftypes[n]
+                if tt <: PossiblyUninitTangent
+                    n <= $N ? tt(fields[n]) : tt()
+                else
+                    fields[n]
+                end
+            end
+        )
+        return TP(NamedTuple{$fnames}(full_fields))
     end
-    return tangent_type(P)(NamedTuple{fieldnames(P)}(fields))
 end
 
-@unstable function build_tangent(::Type{P}, fields...) where {P<:Union{Tuple,NamedTuple}}
-    tangent_type(P) == NoTangent && return NoTangent()
-    isconcretetype(P) && return tangent_type(P)(fields)
-    return __tangent_from_non_concrete(P, fields)
+function build_tangent(
+    ::Type{P}, fields::Vararg{Any,N}
+) where {P<:Union{Tuple,NamedTuple},N}
+    TP = tangent_type(P)
+    TP === NoTangent && return NoTangent()::TP
+    isconcretetype(P) && return TP(fields)
+    return __tangent_from_non_concrete(P, fields)::TP
 end
 
 """
@@ -971,7 +990,7 @@ Required for testing.
 Computes the difference between `p` and `q`, which _must_ be of the same type, `P`.
 Returns a tangent of type `tangent_type(P)`.
 """
-_diff(p::P, q::P) where {P} = _diff_internal(IdDict{Any,Any}(), p, q)
+_diff(p::P, q::P) where {P} = _diff_internal(IdDict{Any,Any}(), p, q)::tangent_type(P)
 
 """
     _diff_internal(c::MaybeCache, p::P, q::P) where {P}
@@ -981,12 +1000,13 @@ aliasing. If `c` is a `NoCache` then assume no circular references or aliasing i
 `p` or `q`.
 """
 function _diff_internal(c::MaybeCache, p::P, q::P) where {P}
-    tangent_type(P) === NoTangent && return NoTangent()
+    TP = tangent_type(P)
+    TP === NoTangent && return NoTangent()
     T = Tangent{NamedTuple{(),Tuple{}}}
-    tangent_type(P) === T && return T((;))
+    TP === T && return T((;))
     key = (p, q)
-    haskey(c, key) && return c[key]::tangent_type(P)
-    return _containerlike_diff(c, p, q)
+    haskey(c, key) && return c[key]::TP
+    return _containerlike_diff(c, p, q)::TP
 end
 _diff_internal(::MaybeCache, p::P, q::P) where {P<:IEEEFloat} = p - q
 function _diff_internal(c::MaybeCache, p::P, q::P) where {P<:SimpleVector}
@@ -1002,25 +1022,54 @@ function _diff_internal(c::MaybeCache, p::P, q::P) where {P<:Union{Tuple,NamedTu
 end
 
 function _containerlike_diff(c::MaybeCache, p::P, q::P) where {P}
-    if ismutabletype(P)
-        t = tangent_type(P)()
-        c[(p, q)] = t
-    end
-    diffed_fields = map(fieldnames(P)) do f
-        if isdefined(p, f) && isdefined(q, f)
-            return _diff_internal(c, getfield(p, f), getfield(q, f))
-        elseif !isdefined(p, f) && !isdefined(q, f)
-            return FieldUndefined()
+    return _containerlike_diff_cartesian(
+        c, p, q, Val(ismutabletype(P)), Val(fieldcount(P)),
+    )
+end
+@generated function _containerlike_diff_cartesian(
+    c::MaybeCache, p::P, q::P, ::Val{mutable}, ::Val{nfield},
+) where {P,mutable,nfield}
+    quote
+        t = if mutable
+            _t = tangent_type(P)()
+            c[(p, q)] = _t
+            _t
         else
-            throw(error("Unhandleable undefinedness"))
+            nothing
         end
+        Base.Cartesian.@nif(
+            $(nfield + 1),
+            n -> let
+                defined_p = isdefined(p, n)
+                defined_q = isdefined(q, n)
+                defined_p != defined_q && throw(error("Unhandleable undefinedness"))
+
+                !defined_p
+            end,
+            # We have found the first undefined field, or,
+            # if n == $(nfield + 1), then we have found the last field,
+            # and all fields are defined.
+            n -> _containerlike_diff_cartesian_internal(
+                Val(n), c, p, q, t, Val(mutable), Val(nfield),
+            )
+        )
     end
-    i = findfirst(==(FieldUndefined()), diffed_fields)
-    diffed_fields = i === nothing ? diffed_fields : diffed_fields[1:(i - 1)]
-    if ismutabletype(P)
-        return _build_tangent(P, t, diffed_fields...)
-    else
-        return build_tangent(P, diffed_fields...)
+end
+@generated function _containerlike_diff_cartesian_internal(
+    ::Val{n}, c::MaybeCache, p::P, q::P, t, ::Val{mutable}, ::Val{nfield}
+) where {P,n,mutable,nfield}
+    quote
+        diffed_fields = Base.Cartesian.@ntuple(
+            $(n - 1),
+            m -> _diff_internal(
+                c, getfield(p, m), getfield(q, m)
+            )::tangent_type(fieldtype(P, m))
+        )
+        if mutable
+            return _build_tangent(P, t, diffed_fields...)
+        else
+            return build_tangent(P, diffed_fields...)
+        end
     end
 end
 
