974de119054854d4a7873e843e2a8b6d5dc28978
MilesCranmer <miles.cranmer@gmail.com>
2025-06-30 13:36:35 +0100

    document DD rrules

--- src/rrules/dispatch_doctor.jl
+++ src/rrules/dispatch_doctor.jl
@@ -1,4 +1,8 @@
 module DispatchDoctorRules
+# This lives here rather than in DispatchDoctor
+# due to the circular dependency. However,
+# the logic here is the same as other DispatchDoctor extensions
+# for, e.g., Enzyme and ChainRulesCore.
 
 import ..@zero_adjoint
 import ..DefaultCtx
@@ -12,18 +16,15 @@ import DispatchDoctor._Utils:
     type_instability,
     type_instability_limit_unions
 
-# Issue #32
 @zero_adjoint DefaultCtx Tuple{typeof(_show_warning),Vararg}
 @zero_adjoint DefaultCtx Tuple{typeof(_construct_pairs),Vararg}
 
-# Issue #46
 @zero_adjoint DefaultCtx Tuple{typeof(specializing_typeof),Any}
 @zero_adjoint DefaultCtx Tuple{typeof(map_specializing_typeof),Vararg}
 @zero_adjoint DefaultCtx Tuple{typeof(_promote_op),Vararg}
 @zero_adjoint DefaultCtx Tuple{typeof(type_instability),Vararg}
 @zero_adjoint DefaultCtx Tuple{typeof(type_instability_limit_unions),Vararg}
 
-# foreigncall expressions
 @zero_adjoint DefaultCtx Tuple{typeof(is_precompiling)}
 @zero_adjoint DefaultCtx Tuple{typeof(checking_enabled)}
 
