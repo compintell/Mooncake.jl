bb9989519d151323295872897dd76bdca713baf5
Hong Ge <3279477+yebai@users.noreply.github.com>
2025-07-02 17:10:25 +0100

    Update src/rrules/builtins.jl
    Signed-off-by: Hong Ge <3279477+yebai@users.noreply.github.com>

--- src/rrules/builtins.jl
+++ src/rrules/builtins.jl
@@ -220,11 +220,13 @@ function rrule!!(f::CoDual{typeof(bitcast)}, t::CoDual{Type{T}}, x) where {T}
     if T <: Ptr && _x isa Ptr
         dv = bitcast(Ptr{tangent_type(eltype(T))}, tangent(x))
     elseif T <: Ptr && _x isa Union{Int,UInt}
-        throw(
-            ArgumentError(
-                "Creating `Core.bitcast`'s primal tangent from a raw integer is unsafe."
-            ),
-        )
+        int2ptr_err_msg =
+            "It is not permissible to bitcast to an Int/UInt type to a Ptr type during AD, as " *
+            "this risks giving the wrong answer, or causing Julia to segfault. " *
+            "If this call to bitcast appears as part of the implementation of a " *
+            "differentiable function, you should write a rule for this function, or modify " *
+            "its implementation to avoid the bitcast."
+        throw(ArgumentError(int2ptr_err_msg))
     else
         dv = NoFData()
     end
