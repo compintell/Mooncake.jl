53fd979eb7f62fec60c2ebefd5c68a3f96811c46
Miles Cranmer <miles.cranmer@gmail.com>
2025-07-03 04:57:08 +0900

    Update src/rrules/performance_patches.jl
    Co-authored-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    Signed-off-by: Miles Cranmer <miles.cranmer@gmail.com>

--- src/rrules/performance_patches.jl
+++ src/rrules/performance_patches.jl
@@ -62,7 +62,7 @@ end
     return test_cases, memory
 end
 
-@unstable @unstable function generate_derived_rrule!!_test_cases(
+@unstable function generate_derived_rrule!!_test_cases(
     rng_ctor, ::Val{:performance_patches}
 )
     return Any[], Any[]
