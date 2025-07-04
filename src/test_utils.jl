06e27056e744a6fddb30fd2dea1178ffb8b6f89b
MilesCranmer <miles.cranmer@gmail.com>
2025-07-04 18:25:50 +0100

    clean up formatting

--- src/test_utils.jl
+++ src/test_utils.jl
@@ -788,12 +788,10 @@ function allow_unstable_given_unstable_type(f::F, ::Type{T}) where {F,T}
     end
 end
 function skip_instability_check(::Type{T}) where {T}
-    if isstructtype(T)
-        return type_instability(T) || any(skip_instability_check, fieldtypes(T))
-    else
-        return type_instability(T)
-    end
+    return type_instability(T) ||
+           (isstructtype(T) && any(skip_instability_check, fieldtypes(T)))
 end
+
 function skip_instability_check(::Type{<:Tangent{Tfields}}) where {Tfields}
     return skip_instability_check(Tfields)
 end
